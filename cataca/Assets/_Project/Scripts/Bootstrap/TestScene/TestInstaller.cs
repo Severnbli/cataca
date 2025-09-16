dbb1b270dcdad7d0da0f7e8790f1fa9a1c3d2152
Usevalad Buben <usevaladbuben@gmail.com>
2025-09-16 22:10:31 +0300

    Add test bootstrap elements

---
+++ cataca/Assets/_Project/Scripts/Bootstrap/TestScene/TestInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace _Project.Scripts.Bootstrap.TestScene
+{
+    public class TestInstaller : MonoInstaller
+    { 
+        public override void InstallBindings()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
