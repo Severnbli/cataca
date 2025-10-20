6d8a3107b8137668abd132797ef2bd2422ddfbf8
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 01:48:15 +0300

    Add Movement and TimeUpdate ECS systems
    Introduces MovementSystem for updating entity positions based on velocity and delta time, and TimeUpdateSystem for managing time-related components in the ECS world. These systems support entity movement and time tracking within the game loop.

---
+++ cataca/Assets/_Project/Scripts/Features/Systems/TimeUpdateSystem.cs
@@ -0,0 +1,36 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Components;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Systems
+{
+    public class TimeUpdateSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<TimeComponent> _timePool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world.Filter<TimeComponent>().End();
+            _timePool = world.GetPool<TimeComponent>();
+            
+            var entity = world.NewEntity();
+            _timePool.Add(entity);
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var entity in _filter)
+            {
+                ref var time = ref _timePool.Get(entity);
+                time.Time = Time.time;
+                time.UnscaledTime = Time.unscaledTime;
+                time.DeltaTime = Time.deltaTime;
+                time.UnscaledDeltaTime = Time.unscaledDeltaTime;
+            }
+        }
+    }
+}
