936256db30a47e7a1cee4b5a8f1707f432d61ab4
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 01:47:58 +0300

    Add TimeComponent struct for time tracking
    Introduces TimeComponent struct to store time-related values such as Time, DeltaTime, UnscaledDeltaTime, and UnscaledTime. This will be used for managing and accessing time data within the project.

---
+++ cataca/Assets/_Project/Scripts/Features/Components/TimeComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Features.Components
+{
+    public struct TimeComponent
+    {
+        public float Time;
+        public float DeltaTime;
+        public float UnscaledDeltaTime;
+        public float UnscaledTime;
+    }
+}
