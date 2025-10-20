c42a4ce0fab22609ecb4186f75cf6f931d36ff61
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-20 23:27:46 +0300

    Add interfaces for systems provider and collector
    Introduced ISystemsProvider and ISystemsProvidersCollector interfaces with Init, Run, and Destroy methods to standardize system lifecycle management.

---
+++ cataca/Assets/_Project/Scripts/Features/Base/Interfaces/ISystemsProvidersCollector.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Scripts.Features.Base.Interfaces
+{
+    public interface ISystemsProvidersCollector
+    {
+        public void Init();
+        public void Run();
+        public void Destroy();
+    }
+}
