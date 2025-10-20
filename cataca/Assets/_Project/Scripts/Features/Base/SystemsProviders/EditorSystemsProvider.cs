f0492d52062d42b314b439292639aa1b40cba96e
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-20 23:29:58 +0300

    Add base and specialized systems providers
    Introduced BaseSystemsProvider as an abstract class for managing EcsSystems lifecycle. Added EditorSystemsProvider for editor-specific systems (with EcsWorldDebugSystem) and GameSystemsProvider for runtime systems. These classes help organize and separate system initialization for different environments.

---
+++ cataca/Assets/_Project/Scripts/Features/Base/SystemsProviders/EditorSystemsProvider.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.UnityEditor;
+
+namespace _Project.Scripts.Features.Base.SystemsProviders
+{
+    public class EditorSystemsProvider : BaseSystemsProvider
+    {
+        public EditorSystemsProvider(EcsWorld world) : base(world)
+        {
+        }
+
+        public override void Init()
+        {
+            Systems
+                .Add(new EcsWorldDebugSystem())
+                .Init();
+        }
+    }
+}
+
+#endif
---
+++ cataca/Assets/_Project/Scripts/Features/Base/SystemsProviders/GameSystemsProvider.cs
@@ -0,0 +1,16 @@
+﻿using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Base.SystemsProviders
+{
+    public class GameSystemsProvider : BaseSystemsProvider
+    {
+        public GameSystemsProvider(EcsWorld world) : base(world)
+        {
+        }
+
+        public override void Init()
+        {
+            Systems.Init();
+        }
+    }
+}
