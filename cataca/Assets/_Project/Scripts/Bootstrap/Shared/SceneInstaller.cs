c976841c38c52cdea0796618a97b68adc8ca47b5
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 01:47:32 +0300

    Add abstract SceneInstaller for DI setup
    Introduces the abstract SceneInstaller class to handle dependency injection bindings for EcsWorld and EcsSystems collections using Zenject. This provides a base for scene-level installers and supports both editor and game collections.

---
+++ cataca/Assets/_Project/Scripts/Bootstrap/Shared/SceneInstaller.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Collections;
+using Leopotam.EcsLite;
+using Zenject;
+
+namespace _Project.Scripts.Bootstrap.Shared
+{
+    public abstract class SceneInstaller : MonoInstaller
+    {
+        public sealed override void InstallBindings()
+        {
+            Container.BindInstance(new EcsWorld()).AsSingle();
+            
+            SetupBindings();
+            SetupCollections();
+        }
+
+        protected abstract void SetupBindings();
+        
+        private void SetupCollections()
+        {
+#if UNITY_EDITOR
+            Container.Bind<EcsSystems>().To<EditorCollection>().AsSingle();
+#endif
+            
+            Container.Bind<EcsSystems>().To<GameCollection>().AsSingle();
+        }
+    }
+}
