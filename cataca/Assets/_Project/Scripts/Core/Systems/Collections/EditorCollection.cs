3280dd28054ac3c10f9446b8f08d3ab275ff3574
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 00:22:29 +0300

    Add EditorCollection and GameCollection system classes
    Introduced EditorCollection and GameCollection classes to manage editor and game systems within the ECS framework. These classes facilitate the addition of systems and include debug support for the Unity Editor.

---
+++ cataca/Assets/_Project/Scripts/Core/Systems/Collections/EditorCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using _Project.Scripts.Core.Systems.Interfaces;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.UnityEditor;
+
+namespace _Project.Scripts.Core.Systems.Collections
+{
+    public class EditorCollection : EcsSystems
+    {
+        public EditorCollection(EcsWorld defaultWorld, IEnumerable<IEditorSystem> systems) : base(defaultWorld)
+        {
+            AddEditorSystems(systems);
+        }
+
+        private void AddEditorSystems(IEnumerable<IEditorSystem> systems)
+        {
+            GetAllSystems().AddRange(systems);
+            GetAllSystems().Add(new EcsSystemsDebugSystem());
+        }
+    }
+}
