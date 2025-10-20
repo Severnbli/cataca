b0b4431ce3dce17c66ceccb72a6eda329dcdca20
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-20 23:30:07 +0300

    Add MonoSystemsProvidersCollector implementation
    Introduced MonoSystemsProvidersCollector, a MonoBehaviour that collects and manages ISystemsProvider instances. It initializes, runs, and destroys all registered providers, integrating with Unity's lifecycle methods. Associated .meta files were also added.

---
+++ cataca/Assets/_Project/Scripts/Features/Base/SystemsProviders/Collectors/MonoSystemsProvidersCollector.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Features.Base.Interfaces;
+using Sirenix.Utilities;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Scripts.Features.Base.SystemsProviders.Collectors
+{
+    public class MonoSystemsProvidersCollector : MonoBehaviour, ISystemsProvidersCollector
+    {
+        private readonly HashSet<ISystemsProvider> _providers = new();
+
+        [Inject]
+        public void Construct(IEnumerable<ISystemsProvider> providers)
+        {
+            _providers.AddRange(providers);
+        }
+        
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Update()
+        {
+            Run();
+        }
+
+        private void OnDestroy()
+        {
+            Destroy();
+        }
+
+        public void Init()
+        {
+            foreach (var p in _providers) p.Init();
+        }
+
+        public void Run()
+        {
+            foreach (var p in _providers) p.Run();
+        }
+
+        public void Destroy()
+        {
+            foreach (var p in _providers) p.Destroy();
+        }
+    }
+}
