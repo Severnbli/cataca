9910e7b50e2bc3c2ce0c413a939263d06e94fbd9
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 16:04:02 +0300

    Refactor MovementSystem to use TimeService
    Replaces the use of TimeComponent and related filter with dependency injection of TimeService for delta time access. Simplifies the system by removing unnecessary filters and pools, improving maintainability and clarity.

--- cataca/Assets/_Project/Scripts/Features/Systems/MovementSystem.cs
+++ cataca/Assets/_Project/Scripts/Features/Systems/MovementSystem.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
-using _Project.Scripts.Core.Systems.Interfaces;
+﻿using _Project.Scripts.Core.Systems.Interfaces;
 using _Project.Scripts.Features.Components;
+using _Project.Scripts.Features.Services;
 using Leopotam.EcsLite;
 
 namespace _Project.Scripts.Features.Systems
@@ -8,37 +8,33 @@ namespace _Project.Scripts.Features.Systems
     public class MovementSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
     {
         private EcsFilter _movablesFilter;
-        private EcsFilter _timeFilter;
-        
         private EcsPool<VelocityComponent> _velocities;
         private EcsPool<TransformComponent> _transforms;
-        private EcsPool<TimeComponent> _times;
+        private readonly TimeService _timeService;
+
+        public MovementSystem(TimeService timeService)
+        {
+            _timeService = timeService;
+        }
         
         public void Init(IEcsSystems systems)
         {
             var world = systems.GetWorld();
 
             _movablesFilter = world.Filter<VelocityComponent>().Inc<TransformComponent>().End();
-            _timeFilter = world.Filter<TimeComponent>().End();
             
             _velocities = world.GetPool<VelocityComponent>();
             _transforms = world.GetPool<TransformComponent>();
-            _times = world.GetPool<TimeComponent>();
         }
         
         public void Run(IEcsSystems systems)
         {
-            if (_timeFilter.GetEntitiesCount() == 0) return;
-            
-            var time = _times[_timeFilter[0]]
-            
-            
             foreach (var entity in _movablesFilter)
             {
                 var velocity = _velocities.Get(entity);
                 var transform = _transforms.Get(entity);
                 
-                transform.Transform.position += velocity.Value * time.DeltaTime;
+                transform.Transform.position += velocity.Value * _timeService.DeltaTime;
             }
         }
     }
