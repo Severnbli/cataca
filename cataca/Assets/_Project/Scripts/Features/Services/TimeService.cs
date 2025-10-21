7129f67b1382263e8abdcf991c9970ec688c84b4
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 16:01:29 +0300

    Add TimeService class for time management
    Introduced a new TimeService class to encapsulate time-related properties such as Time, DeltaTime, UnscaledDeltaTime, and UnscaledTime. This service can be used to manage and access time values throughout the project.

---
+++ cataca/Assets/_Project/Scripts/Features/Services/TimeService.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.Features.Services
+{
+    public sealed class TimeService
+    {
+        public float Time;
+        public float DeltaTime;
+        public float UnscaledDeltaTime;
+        public float UnscaledTime;
+    }
+}
