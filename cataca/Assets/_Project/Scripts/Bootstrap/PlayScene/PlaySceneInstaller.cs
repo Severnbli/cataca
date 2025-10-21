7655c26c7964e87a8ed1e751b080f23d7b9c9ccb
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 16:04:25 +0300

    Bind new services and systems in PlaySceneInstaller
    Added bindings for TimeService, MovementSystem, and TimeServiceUpdateSystem in PlaySceneInstaller to ensure they are available as singletons and their interfaces are properly registered.

--- cataca/Assets/_Project/Scripts/Bootstrap/PlayScene/PlaySceneInstaller.cs
+++ cataca/Assets/_Project/Scripts/Bootstrap/PlayScene/PlaySceneInstaller.cs
@@ -1,4 +1,6 @@
 ﻿using _Project.Scripts.Bootstrap.Shared;
+using _Project.Scripts.Features.Services;
+using _Project.Scripts.Features.Systems;
 
 namespace _Project.Scripts.Bootstrap.PlayScene
 {
@@ -6,6 +8,9 @@ namespace _Project.Scripts.Bootstrap.PlayScene
     {
         protected override void SetupBindings()
         {
+            Container.Bind<TimeService>().AsSingle();
+            Container.BindInterfacesTo<MovementSystem>().AsSingle();
+            Container.BindInterfacesTo<TimeServiceUpdateSystem>().AsSingle();
         }
     }
 }
