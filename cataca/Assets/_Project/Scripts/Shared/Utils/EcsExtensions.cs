ca2d5003e24ae9513017f221af131977d4d60ffb
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 16:03:32 +0300

    Add EcsExtensions utility with TryGetFirst method
    Introduces a static extension method for EcsFilter to retrieve the first entity if available. This utility simplifies checking and accessing the first entity in a filter.

---
+++ cataca/Assets/_Project/Scripts/Shared/Utils/EcsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Shared.Utils
+{
+    public static class EcsExtensions
+    {
+        public static bool TryGetFirst(this EcsFilter filter, out int entity)
+        {
+            entity = -1;
+            if (filter.GetEntitiesCount() == 0) return false;
+
+            entity = filter.GetRawEntities()[0];
+            return true;
+        }
+    }
+}
