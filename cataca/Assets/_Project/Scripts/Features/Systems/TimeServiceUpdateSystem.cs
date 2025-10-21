e316b397be4cb7adf897b1b6ccd0e3efcc567a5e
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-21 16:03:20 +0300

    Refactor time update to use TimeService
    Removed TimeComponent and TimeUpdateSystem, replacing them with TimeServiceUpdateSystem that updates time values directly in TimeService. This simplifies time management by centralizing time data and removing the ECS component.

---
+++ cataca/Assets/_Project/Scripts/Features/Systems/TimeServiceUpdateSystem.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Components;
+using _Project.Scripts.Features.Services;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Systems
+{
+    public class TimeServiceUpdateSystem : IEcsRunSystem, IEcsGameSystem
+    {
+        private readonly TimeService _timeService;
+
+        public TimeServiceUpdateSystem(TimeService timeService)
+        {
+            _timeService = timeService;
+        }
+        
+        public void Run(IEcsSystems systems)
+        {
+            _timeService.Time = Time.time;
+            _timeService.UnscaledTime = Time.unscaledTime;
+            _timeService.DeltaTime = Time.deltaTime;
+            _timeService.UnscaledDeltaTime = Time.unscaledDeltaTime;
+        }
+    }
+}
