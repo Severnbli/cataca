e6e3a4d6f93fd39a9ffe806aa42340ea6895704b
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-24 18:36:08 +0300

    Add GravityComponent and its converter
    Introduced GravityComponent struct to represent gravity value and added GravityComponentConverter for scene-to-entity conversion in the physics system.

---
+++ cataca/Assets/_Project/Scripts/Features/Systems/Physics/Components/GravityComponentConverter.cs
@@ -0,0 +1,9 @@
+﻿using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Systems.Physics.Components
+{
+    public class GravityComponentConverter : ComponentConverter<GravityComponent>
+    {
+        
+    }
+}
