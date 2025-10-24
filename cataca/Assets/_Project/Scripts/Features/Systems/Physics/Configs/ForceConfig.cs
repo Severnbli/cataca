90140bcd232644459e918aaa499366bd4962ab95
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-24 18:33:41 +0300

    Add ForceConfig base class for physics system
    Introduced the abstract ForceConfig<T> class as a ScriptableObjectInstaller for configuring force parameters in the physics system. Includes fields for enabling, value, and direction, and sets up Zenject bindings.

---
+++ cataca/Assets/_Project/Scripts/Features/Systems/Physics/Configs/ForceConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace _Project.Scripts.Features.Systems.Physics.Configs
+{
+    public abstract class ForceConfig<T> : ScriptableObjectInstaller where T : ForceConfig<T>
+    {
+        public bool isEnabled;
+        public float value;
+        public Vector3 direction;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<T>().To<T>().AsSingle();
+        }
+    }
+}
