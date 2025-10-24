9bd6a4d0ac2fed420c6c6fe7296f2e17367d0ec5
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-25 01:15:35 +0300

    Change name of action from move to walk

--- cataca/Assets/_Project/Scripts/Features/Input/Configs/InputControlsConfig.cs
+++ cataca/Assets/_Project/Scripts/Features/Input/Configs/InputControlsConfig.cs
@@ -6,14 +6,14 @@ namespace _Project.Scripts.Features.Input.Configs
 {
     public class InputControlsConfig : ScriptableObjectAutoInstaller<InputControlsConfig>
     {
-        [SerializeField] private InputActionReference move;
+        [SerializeField] private InputActionReference walk;
         [SerializeField] private InputActionReference jump;
         [SerializeField] private InputActionReference dash;
         [SerializeField] private InputActionReference position;
         [SerializeField] private InputActionReference rotation;
         [SerializeField] private InputActionReference scale;
         
-        public InputActionReference Move => move;
+        public InputActionReference Walk => walk;
         public InputActionReference Jump => jump;
         public InputActionReference Dash => dash;
         public InputActionReference Position => position;
