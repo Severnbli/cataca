eff28e2e88ceebe4af6732e5605478ba70d8213e
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-25 01:30:50 +0300

    Add PlayerMarker and its converter
    Introduced the PlayerMarker struct and PlayerMarkerConverter class to the Shared features. These additions enable marking player entities and converting PlayerMarker components from scene to entity.

---
+++ cataca/Assets/_Project/Scripts/Features/Shared/Converters/PlayerMarkerConverter.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Shared.Markers;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Shared.Converters
+{
+    public class PlayerMarkerConverter : ComponentConverter<PlayerMarker>
+    {
+    }
+}
