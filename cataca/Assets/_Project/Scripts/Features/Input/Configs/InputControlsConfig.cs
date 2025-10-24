44767f57718150898f28d2703e341c673b4480a1
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-25 01:14:23 +0300

    Add InputControlsConfig ScriptableObject
    Introduced InputControlsConfig as a ScriptableObject for managing input action references such as move, jump, dash, position, rotation, and scale.

---
+++ cataca/Assets/_Project/Scripts/Features/Input/Configs/InputControlsConfig.cs
@@ -0,0 +1,23 @@
+﻿using _Project.Scripts.Shared.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Project.Scripts.Features.Input.Configs
+{
+    public class InputControlsConfig : ScriptableObjectAutoInstaller<InputControlsConfig>
+    {
+        [SerializeField] private InputActionReference move;
+        [SerializeField] private InputActionReference jump;
+        [SerializeField] private InputActionReference dash;
+        [SerializeField] private InputActionReference position;
+        [SerializeField] private InputActionReference rotation;
+        [SerializeField] private InputActionReference scale;
+        
+        public InputActionReference Move => move;
+        public InputActionReference Jump => jump;
+        public InputActionReference Dash => dash;
+        public InputActionReference Position => position;
+        public InputActionReference Rotation => rotation;
+        public InputActionReference Scale => scale;
+    }
+}
