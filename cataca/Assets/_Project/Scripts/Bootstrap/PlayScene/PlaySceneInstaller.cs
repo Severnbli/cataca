9c91b790c2298ecfab9d89dd2c1a62868f7b2371
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-24 22:56:57 +0300

    Reorganize project
    1) Delete useless files: will be used built-in physics logic
    2) Create folders for each feature instead of monolith "Systems", "Services", "Components"
    3) Fix dependencies & namespaces throughout project

--- cataca/Assets/_Project/Scripts/Bootstrap/PlayScene/PlaySceneInstaller.cs
+++ cataca/Assets/_Project/Scripts/Bootstrap/PlayScene/PlaySceneInstaller.cs
@@ -1,7 +1,6 @@
 ﻿using _Project.Scripts.Bootstrap.Shared;
-using _Project.Scripts.Features.Services;
-using _Project.Scripts.Features.Systems;
-using _Project.Scripts.Features.Systems.Physics;
+using _Project.Scripts.Features.Time.Services;
+using _Project.Scripts.Features.Time.Systems;
 
 namespace _Project.Scripts.Bootstrap.PlayScene
 {
@@ -10,9 +9,7 @@ namespace _Project.Scripts.Bootstrap.PlayScene
         protected override void SetupBindings()
         {
             Container.Bind<TimeService>().AsSingle();
-            Container.BindInterfacesTo<MovementSystem>().AsSingle();
             Container.BindInterfacesTo<TimeServiceUpdateSystem>().AsSingle();
-            Container.BindInterfacesTo<GravityApplierSystem>().AsSingle();
         }
     }
 }
