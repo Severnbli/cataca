67029926a745b389cefb52a992cfe7fee8b72845
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-26 00:32:35 +0300

    Add Rigidbody2D extension for walking logic
    Introduces a static extension method ProcessWalk for Rigidbody2D, allowing simplified application of walking force using a walk factor. This utility aids in character movement handling.

---
+++ cataca/Assets/_Project/Scripts/Shared/Utils/Rigidbody2DExtensions.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Features.Physics.Characters.Components;
+using UnityEngine;
+
+namespace _Project.Scripts.Shared.Utils
+{
+    public static class Rigidbody2DExtensions
+    {
+        public static void ProcessWalk(this Rigidbody2D rigidbody2D, float force, float walkFactor)
+        {
+            rigidbody2D.linearVelocity = new Vector2(force * walkFactor, rigidbody2D.linearVelocity.y);
+        }
+    }
+}
