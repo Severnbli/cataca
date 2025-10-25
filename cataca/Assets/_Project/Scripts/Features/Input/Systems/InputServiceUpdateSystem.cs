d3c8d11631677401b835b34bd7228ed4007c37f8
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-25 13:08:17 +0300

    Add InputServiceUpdateSystem for input handling
    Introduced InputServiceUpdateSystem to update input states in the InputService using values from InputControlsConfig. This system reads input actions for walk, jump, dash, position, rotation, and scale, and updates the service accordingly.

---
+++ cataca/Assets/_Project/Scripts/Features/Input/Systems/InputServiceUpdateSystem.cs
@@ -0,0 +1,30 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Input.Configs;
+using _Project.Scripts.Features.Input.Services;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Input.Systems
+{
+    public class InputServiceUpdateSystem : IEcsRunSystem, IEcsGameSystem
+    {
+        public InputServiceUpdateSystem(InputService inputService, InputControlsConfig controlsConfig)
+        {
+            _inputService = inputService;
+            _controlsConfig = controlsConfig;
+        }
+        
+        private InputService _inputService;
+        private InputControlsConfig _controlsConfig;
+            
+        public void Run(IEcsSystems systems)
+        {
+            _inputService.Walk = _controlsConfig.Walk.action.ReadValue<Vector2>();
+            _inputService.Jump = _controlsConfig.Jump.action.triggered;
+            _inputService.Dash = _controlsConfig.Dash.action.triggered;
+            _inputService.Position = _controlsConfig.Position.action.triggered;
+            _inputService.Rotation = _controlsConfig.Rotation.action.triggered;
+            _inputService.Scale = _controlsConfig.Scale.action.triggered;
+        }
+    }
+}
