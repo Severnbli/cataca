81fbc277a65f4d01aff88b11e6e46c1697ae7fe7
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-26 00:47:27 +0300

    Add ProcessPush and ProcessUpperPush to Rigidbody2DExtensions
    Introduced two new extension methods for Rigidbody2D: ProcessPush, which applies an impulse force in a given direction, and ProcessUpperPush, which ensures the force is applied with a positive Y component.

--- cataca/Assets/_Project/Scripts/Shared/Utils/Rigidbody2DExtensions.cs
+++ cataca/Assets/_Project/Scripts/Shared/Utils/Rigidbody2DExtensions.cs
@@ -1,5 +1,4 @@
-﻿using _Project.Scripts.Features.Physics.Characters.Components;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace _Project.Scripts.Shared.Utils
 {
@@ -8,6 +7,17 @@ namespace _Project.Scripts.Shared.Utils
         public static void ProcessWalk(this Rigidbody2D rigidbody2D, float force, float walkFactor)
         {
             rigidbody2D.linearVelocity = new Vector2(force * walkFactor, rigidbody2D.linearVelocity.y);
+        }
+
+        public static void ProcessPush(this Rigidbody2D rigidbody2D, float force, Vector2 direction)
+        {
+            rigidbody2D.AddForce(direction.normalized * force, ForceMode2D.Impulse);
+        }
+
+        public static void ProcessUpperPush(this Rigidbody2D rigidbody2D, float force, Vector2 direction)
+        {
+            var upperDirection = new Vector2(direction.x, Mathf.Abs(direction.y));
+            rigidbody2D.ProcessPush(force, upperDirection);
         }
     }
 }
