567c6bda7a5b9a86e018ffa94759f5c0489a79c2
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-26 01:48:18 +0300

    Add Vector2Extensions with GetNearestDiagonal method
    Introduces a static extension method for Vector2 that returns the nearest diagonal direction, normalized. This utility can be used to simplify directional calculations in the project.

---
+++ cataca/Assets/_Project/Scripts/Shared/Utils/Vector2Extensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Shared.Utils
+{
+    public static class Vector2Extensions
+    {
+        public static Vector2 GetNearestDiagonal(this Vector2 vector)
+        {
+            var result = new Vector2(Mathf.Sign(vector.x), Mathf.Sign(vector.y));
+            return result.normalized;
+        }
+    }
+}
