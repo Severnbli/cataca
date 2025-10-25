2928d5f825ca1a265a0484ad0e055c1d6ae1969a
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-26 00:01:46 +0300

    Add character movement components
    Introduced DashComponent, JumpComponent, and WalkComponent structs for character movement features, each with relevant properties.

---
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Components/JumpComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Project.Scripts.Features.Physics.Characters.Components
+{
+    [Serializable]
+    public struct JumpComponent
+    {
+        public bool Enabled;
+        public float Force;
+        public int MaxCount;
+    }
+}
