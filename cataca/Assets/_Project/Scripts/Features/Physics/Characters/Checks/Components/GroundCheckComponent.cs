3967ceb9db9605a0ff02b9f68a874cb712a7fb32
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 01:47:56 +0300

    Add GroundCheckComponent struct for character physics
    Introduced the GroundCheckComponent struct to support ground checking in character physics.

---
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Checks/Components/GroundCheckComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Physics.Characters.Checks.Components
+{
+    [Serializable]
+    public struct GroundCheckComponent
+    {
+        public Transform Transform;
+        public LayerMask Layer;
+        public float Distance;
+    }
+}
