4cb56b36d4bbf4a671cead3a4f144e22f8795b13
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 01:48:55 +0300

    Add Physics2DExtensions utility class
    Introduces a static class with an IsHitCollider method to simplify 2D raycast hit checks in Unity. This utility helps determine if a collider is hit given a position, direction, distance, and layer mask.

---
+++ cataca/Assets/_Project/Scripts/Shared/Utils/Physics2DExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Shared.Utils
+{
+    public static class Physics2DExtensions
+    {
+        public static bool IsHitCollider(Vector2 position, Vector2 vector2, float distance, LayerMask layerMask)
+        {
+            var hit = Physics2D.Raycast(position, vector2, distance, layerMask);
+            return hit.collider is not null;
+        }
+    }
+}
