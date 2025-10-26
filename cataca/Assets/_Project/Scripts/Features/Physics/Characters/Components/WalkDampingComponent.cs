ee4e89fa3e66b6010c08fa67160a799089b67c8c
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 00:42:41 +0300

    Add WalkDampingComponent struct
    Introduces WalkDampingComponent with Force and Factor fields for character physics handling.

---
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Components/WalkDampingComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace _Project.Scripts.Features.Physics.Characters.Components
+{
+    public struct WalkDampingComponent
+    {
+        public float Force;
+        public float Factor;
+    }
+}
