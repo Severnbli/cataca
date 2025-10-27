637ca60c55051814f72016174c92b6908d041a32
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 19:45:05 +0300

    Add Disabled flag to GroundCheckComponent
    Introduced a public boolean 'Disabled' field to the GroundCheckComponent struct, allowing ground checks to be enabled or disabled as needed.

--- cataca/Assets/_Project/Scripts/Features/Physics/Characters/Checks/Components/GroundCheckComponent.cs
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Checks/Components/GroundCheckComponent.cs
@@ -6,6 +6,7 @@ namespace _Project.Scripts.Features.Physics.Characters.Checks.Components
     [Serializable]
     public struct GroundCheckComponent
     {
+        public bool Disabled;
         public Transform Transform;
         public LayerMask Layer;
         public float Distance;
