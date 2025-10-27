c4bba3376872936e99a75c84323d2963c6a75326
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 19:45:25 +0300

    Add GroundCheckTimeBlockComponent struct
    Introduces GroundCheckTimeBlockComponent to manage block timing for ground checks in character physics. Includes BlockTime and a non-serialized ElapsedTime field.

---
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Checks/Components/GroundCheckTimeBlockComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Project.Scripts.Features.Physics.Characters.Checks.Components
+{
+    public struct GroundCheckTimeBlockComponent
+    {
+        public float BlockTime;
+        [NonSerialized] public float ElapsedTime;
+    }
+}
