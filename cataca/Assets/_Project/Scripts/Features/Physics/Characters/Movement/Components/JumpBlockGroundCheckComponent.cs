791a5321f12927a9841da146a7aa32ad33087afc
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 19:46:09 +0300

    Add JumpBlockGroundCheckComponent struct
    Introduces a serializable struct for character movement, containing a BlockTime field to manage jump blocking based on ground checks.

---
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Movement/Components/JumpBlockGroundCheckComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _Project.Scripts.Features.Physics.Characters.Movement.Components
+{
+    [Serializable]
+    public struct JumpBlockGroundCheckComponent
+    {
+        public float BlockTime;
+    }
+}
