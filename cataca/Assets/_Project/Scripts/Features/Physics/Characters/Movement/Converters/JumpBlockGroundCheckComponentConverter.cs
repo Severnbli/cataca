f80a7bbb09b7bcea3e4d08c293ca85a3e8b60548
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-27 19:46:32 +0300

    Add JumpBlockGroundCheckComponentConverter class
    Introduced JumpBlockGroundCheckComponentConverter to handle conversion of JumpBlockGroundCheckComponent for scene-to-entity workflows.

---
+++ cataca/Assets/_Project/Scripts/Features/Physics/Characters/Movement/Converters/JumpBlockGroundCheckComponentConverter.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Physics.Characters.Movement.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Physics.Characters.Movement.Converters
+{
+    public class JumpBlockGroundCheckComponentConverter : ComponentConverter<JumpBlockGroundCheckComponent>
+    {
+    }
+}
