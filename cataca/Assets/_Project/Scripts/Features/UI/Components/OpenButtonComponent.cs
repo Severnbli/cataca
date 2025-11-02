a12f4cbeeadea6b4c7f522bfe967da0a3cecd68c
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 18:16:46 +0300

    Add OpenButtonComponent and its converter
    Introduced OpenButtonComponent struct for UI button handling and created OpenButtonConverter for converting this component.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Components/OpenButtonComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Features.UI.Components
+{
+    [Serializable]
+    public struct OpenButtonComponent
+    {
+        public Button Button;
+    }
+}
