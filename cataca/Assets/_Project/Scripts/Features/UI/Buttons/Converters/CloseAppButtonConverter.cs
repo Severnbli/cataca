4033d0159cdcd59a3541bd5e41c4e0615f9b6d8b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 19:17:35 +0300

    Add CloseAppButton marker and converter classes
    Introduced CloseAppButtonMarker struct and CloseAppButtonConverter class to support close application button functionality in the UI.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Buttons/Converters/CloseAppButtonConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.UI.Buttons.Markers;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.UI.Buttons.Converters
+{
+    public class CloseAppButtonConverter : ComponentConverter<CloseAppButtonMarker>
+    {
+        
+    }
+}
