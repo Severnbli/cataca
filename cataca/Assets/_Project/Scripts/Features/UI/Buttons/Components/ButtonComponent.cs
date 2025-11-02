f779c85fc263d4ad870c62b4e0676d7d9a21fcd9
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 19:02:31 +0300

    Add ButtonComponent and ButtonConverter for UI buttons
    Introduced ButtonComponent struct to encapsulate UnityEngine.UI.Button and a ButtonConverter class for converting ButtonComponent instances.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Buttons/Components/ButtonComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Features.UI.Buttons.Components
+{
+    [Serializable]
+    public struct ButtonComponent
+    {
+        public Button Button;
+    }
+}
