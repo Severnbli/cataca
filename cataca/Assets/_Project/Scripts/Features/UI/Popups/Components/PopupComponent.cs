37146d8c3195e7411a44a5c4a264a9be07bb8117
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 18:21:15 +0300

    Add PopupComponent and PopupConverter for UI popups
    Introduced PopupComponent struct and PopupConverter class to support UI popup features.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Popups/Components/PopupComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Features.UI.Popups.Components
+{
+    [Serializable]
+    public struct PopupComponent
+    {
+        public Image Fade;
+        public GameObject Body;
+    }
+}
---
+++ cataca/Assets/_Project/Scripts/Features/UI/Popups/Converters/PopupConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.UI.Popups.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.UI.Popups.Converters
+{
+    public class PopupConverter : ComponentConverter<PopupComponent>
+    {
+        
+    }
+}
