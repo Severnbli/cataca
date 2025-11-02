8720df5cc01707990f694db4187281b94082e3fb
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 18:16:46 +0300

    Add OpenButtonComponent and its converter
    Introduced OpenButtonComponent struct for UI button handling and created OpenButtonConverter for converting this component.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Converters/OpenButtonConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.UI.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.UI.Converters
+{
+    public class OpenButtonConverter : ComponentConverter<OpenButtonComponent>
+    {
+        
+    }
+}
