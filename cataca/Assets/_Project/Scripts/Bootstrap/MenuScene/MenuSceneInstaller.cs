e84b06cdd08228d18f5d96eda35182fd3c6e1386
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 20:18:01 +0300

    Add MenuSceneInstaller for menu scene system binding
    Introduces MenuSceneInstaller, which binds CloseAppByButtonSystem as a singleton for the menu scene.

---
+++ cataca/Assets/_Project/Scripts/Bootstrap/MenuScene/MenuSceneInstaller.cs
@@ -0,0 +1,13 @@
+﻿using _Project.Scripts.Bootstrap.Shared;
+using _Project.Scripts.Features.UI.Buttons.Systems;
+
+namespace _Project.Scripts.Bootstrap.MenuScene
+{
+    public class MenuSceneInstaller : SceneInstaller
+    {
+        protected override void BindSystems()
+        {
+            Container.BindInterfacesTo<CloseAppByButtonSystem>().AsSingle();
+        }
+    }
+}
