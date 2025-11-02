610cf6eec3dd6d29b1c9c41f28c1e48939eacedb
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 18:16:17 +0300

    Add CloseButtonComponent and converter for UI
    Introduces CloseButtonComponent struct for UI button handling and a corresponding CloseButtonConverter for entity conversion.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Components/CloseButtonComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Features.UI.Components
+{
+    [Serializable]
+    public struct CloseButtonComponent
+    {
+        public Button Button;
+    }
+}
---
+++ cataca/Assets/_Project/Scripts/Features/UI/Converters/CloseButtonConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.UI.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.UI.Converters
+{
+    public class CloseButtonConverter : ComponentConverter<CloseButtonComponent>
+    {
+        
+    }
+}
