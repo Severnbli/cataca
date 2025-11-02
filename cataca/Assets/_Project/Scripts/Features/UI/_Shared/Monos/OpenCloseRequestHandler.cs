4d1cf3cdc0a73848024949c4a728d19f29fcb252
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 21:03:41 +0300

    Add OpenCloseRequestHandler MonoBehaviour

---
+++ cataca/Assets/_Project/Scripts/Features/UI/_Shared/Monos/OpenCloseRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.UI._Shared.Monos
+{
+    public class OpenCloseRequestHandler : MonoBehaviour
+    {
+        [NonSerialized] public bool openRequested;
+        [NonSerialized] public bool closeRequested;
+    }
+}
