40eedb60cf736c18c1a79ab63210be9c2412f557
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-02 21:04:03 +0300

    Add CloseButtonComponent and converter for UI buttons

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Buttons/Components/CloseButtonComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using _Project.Scripts.Features.UI._Shared.Monos;
+
+namespace _Project.Scripts.Features.UI.Buttons.Components
+{
+    [Serializable]
+    public struct CloseButtonComponent
+    {
+        public OpenCloseRequestHandler RequestHandler;
+    }
+}
