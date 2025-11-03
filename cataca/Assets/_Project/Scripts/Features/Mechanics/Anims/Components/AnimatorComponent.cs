84eba36199817b4c6812acdc54b75170e3597eea
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-03 23:09:53 +0300

    Add AnimatorComponent and its converter

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Anims/Components/AnimatorComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Anims.Components
+{
+    [Serializable]
+    public struct AnimatorComponent
+    {
+        public Animator Animator;
+    }
+}
