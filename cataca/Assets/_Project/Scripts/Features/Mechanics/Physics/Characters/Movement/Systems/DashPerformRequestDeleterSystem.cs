2383220ef37ae472174443a4f67694fd50c754f9
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-03 23:09:13 +0300

    Add DashPerformRequestDeleterSystem

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Characters/Movement/Systems/DashPerformRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Physics.Characters.Movement.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Characters.Movement.Systems
+{
+    public class DashPerformRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<DashPerformRequest> _dashPerformRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<DashPerformRequest>()
+                .End();
+            
+            _dashPerformRequestPool = world.GetPool<DashPerformRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _dashPerformRequestPool.Del(e);
+        }
+    }
+}
