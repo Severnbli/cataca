ed501d485fa02a326e05ba85001e808b4ba9d8e4
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-04 00:07:49 +0300

    Dash animation trigger system setup

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/PlayerDashAnimationTriggerSystem.cs
@@ -0,0 +1,43 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Anims.Components;
+using _Project.Scripts.Features.Mechanics.Physics.Characters.Movement.Requests;
+using _Project.Scripts.Features.Mechanics.Player.Configs;
+using _Project.Scripts.Features.Mechanics.Player.Markers;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Player.Systems
+{
+    public class PlayerDashAnimationTriggerSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        public PlayerDashAnimationTriggerSystem(PlayerAnimationConfig config)
+        {
+            _config = config;
+        }
+        
+        private PlayerAnimationConfig _config;
+        private EcsFilter _filter;
+        private EcsPool<AnimatorComponent> _animatorPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<DashPerformRequest>()
+                .Inc<AnimatorComponent>()
+                .Inc<PlayerMarker>()
+                .End();
+            
+            _animatorPool = world.GetPool<AnimatorComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var animator = ref _animatorPool.Get(e);
+                animator.Animator.SetTrigger(_config.DashTrigger);
+            }
+        }
+    }
+}
