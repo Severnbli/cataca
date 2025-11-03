7003c958fb70de81da3f6b31fb8c129d6529e352
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-03 23:09:22 +0300

    Add JumpPerformRequestDeleterSystem

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Characters/Movement/Systems/JumpPerformRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Physics.Characters.Movement.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Characters.Movement.Systems
+{
+    public class JumpPerformRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<JumpPerformRequest> _jumpPerformRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<JumpPerformRequest>()
+                .End();
+            
+            _jumpPerformRequestPool = world.GetPool<JumpPerformRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _jumpPerformRequestPool.Del(e);
+        }
+    }
+}
