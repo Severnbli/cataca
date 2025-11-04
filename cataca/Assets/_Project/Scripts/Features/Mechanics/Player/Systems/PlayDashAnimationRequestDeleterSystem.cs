25aa43164e362db2cdbb492baf3a4d7c1b59beb5
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-04 15:30:40 +0300

    Add systems to delete play animation requests

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/PlayDashAnimationRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Player.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Player.Systems
+{
+    public class PlayDashAnimationRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<PlayDashAnimationRequest> _playDashAnimationRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<PlayDashAnimationRequest>()
+                .End();
+            
+            _playDashAnimationRequestPool = world.GetPool<PlayDashAnimationRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _playDashAnimationRequestPool.Del(e);
+        }
+    }
+}
