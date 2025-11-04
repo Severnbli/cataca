e57ae469dfd9b635814c9a59a81bd84d3ed73538
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-04 17:50:13 +0300

    Add JsonUtils
    This utils based on Newtonsoft Json package

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/JsonUtils.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace _Project.Scripts._Shared.Utils
+{
+    public static class JsonUtils
+    {
+        public static string ToJson<T>(T obj, bool pretty = false)
+        {
+            return JsonConvert.SerializeObject(obj, pretty ? Formatting.Indented : Formatting.None);
+        }
+
+        public static T FromJson<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
