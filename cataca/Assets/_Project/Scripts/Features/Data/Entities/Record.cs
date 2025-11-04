692d72327d5c3574dfa2230ec749cddc352501f1
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-04 17:53:05 +0300

    Add entities
    Add Level, Record classes that describe object that will be stored

---
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _Project.Scripts.Features.Data.Entities
+{
+    [Serializable]
+    public class Record
+    {
+        private string id;
+
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+    }
+}
