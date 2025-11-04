1e1722ddadffe7ea38c1559b2a17ea0badf47698
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-04 16:03:35 +0300

    Add  BuiltInStorageConfig
    This class encapsulate ability to setup keys to access storages

---
+++ cataca/Assets/_Project/Scripts/Features/Data/Storages/BuiltIn/Configs/BuiltInStorageConfig.cs
@@ -0,0 +1,42 @@
+﻿using _Project.Scripts._Shared.ScriptableObjects;
+using _Project.Scripts._Shared.Utils;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Data.Storages.BuiltIn.Configs
+{
+    public class BuiltInStorageConfig : ScriptableObjectAutoInstaller<BuiltInStorageConfig>
+    {
+        [SerializeField] private string _levels;
+        [SerializeField] private string _records;
+        
+        public string Levels => _levels;
+        public string Records => _records;
+
+#if UNITY_EDITOR
+        [PropertySpace(10)]
+        [Button]
+        private void ResetLevels()
+        {
+            PlayerPrefs.DeleteKey(_levels);
+        }
+        
+        [PropertySpace(10)]
+        [Button]
+        private void ResetRecords()
+        {
+            PlayerPrefs.DeleteKey(_records);
+        }
+        
+        [PropertySpace(10)]
+        [Button]
+        private void ResetAll()
+        {
+            if (EditorHelpers.Confirm("Warning! It will reset ALL keys even if they are not specified."))
+            {
+                PlayerPrefs.DeleteAll();
+            }
+        }
+#endif
+    }
+}
