641748ab49c9d72a72618696e77819af466a8bff
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-04 15:59:16 +0300

    Add EditorHelpers

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/EditorHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts._Shared.Utils
+{
+    public static class EditorHelpers
+    {
+        public static bool Confirm(string msg)
+        {
+            return UnityEditor.EditorUtility.DisplayDialog("Confirm Action", msg, "Yes", "Cancel");
+        }
+    }
+}
