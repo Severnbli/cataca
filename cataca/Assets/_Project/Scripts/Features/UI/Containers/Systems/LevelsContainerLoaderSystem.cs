7f5e451c4b9a8b99777acf64c9d6ae2c590382c2
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-07 16:49:58 +0300

    feat: Add LevelButton and LevelsContainer systems (dev)
    Introduces new scripts for UI elements and systems.
    
    - Adds `LevelButton` MonoBehaviour for UI buttons.
    - Implements `LevelsContainerMarker` as a struct.
    - Creates `LevelsContainerMarkerConverter` for component conversion.
    - Adds `LevelsContainerLoaderSystem` to handle container loading.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Containers/Systems/LevelsContainerLoaderSystem.cs
@@ -0,0 +1,30 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Data.Storages.BuiltIn.Configs;
+using _Project.Scripts.Features.UI.Containers.Components;
+using _Project.Scripts.Features.UI.Containers.Markers;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.UI.Containers.Systems
+{
+    public class LevelsContainerLoaderSystem : IEcsInitSystem, IEcsGameSystem
+    {
+        public LevelsContainerLoaderSystem(BuiltInStorageConfig config)
+        {
+            _config = config;
+        }
+        
+        private BuiltInStorageConfig _config;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            var filter = world
+                .Filter<ContainerComponent>()
+                .Inc<LevelsContainerMarker>()
+                .End();
+            
+            
+        }
+    }
+}
