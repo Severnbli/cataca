d88d02c73fa3c8ed0030bc5b8d6079f8d2c9941b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-07 16:49:41 +0300

    feat: Add StorageUtils with LoadLevels method (dev)
    Introduces a new utility class `StorageUtils` to handle storage operations.
    This commit includes the `StorageUtils` class with a placeholder `LoadLevels` method. This method is intended to be implemented in subsequent commits to load level data from configurations.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/StorageUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Features.Data.Entities;
+using _Project.Scripts.Features.Data.Storages.BuiltIn.Configs;
+
+namespace _Project.Scripts._Shared.Utils
+{
+    public static class StorageUtils
+    {
+        public static List<Level> LoadLevels(BuiltInStorageConfig config)
+        {
+            return new List<Level>();
+        }
+    }
+}
