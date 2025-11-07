4c2abfb4a2fc1c66b8ecd5d5864717d07a6d4567
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-07 16:39:12 +0300

    feat: Add ContainerComponent and ContainerConverter (dev)
    Introduces a new ContainerComponent struct and its corresponding Converter.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Containers/Converters/ContainerConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.UI.Containers.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.UI.Containers.Converters
+{
+    public class ContainerConverter : ComponentConverter<ContainerComponent>
+    {
+        
+    }
+}
