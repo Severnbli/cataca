b103dfcbf2dbd5d43b673d249142b0376a0d0fdd
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-07 16:39:12 +0300

    feat: Add ContainerComponent and ContainerConverter (dev)
    Introduces a new ContainerComponent struct and its corresponding Converter.

---
+++ cataca/Assets/_Project/Scripts/Features/UI/Containers/Components/ContainerComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.UI.Containers.Components
+{
+    [Serializable]
+    public struct ContainerComponent
+    {
+        public GameObject Parent;
+    }
+}
