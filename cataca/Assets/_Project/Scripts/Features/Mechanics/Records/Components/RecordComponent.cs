4fa75981a845ba93d472a5642ef80c73b0043da1
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 18:43:44 +0300

    feat: Add RecordComponent struct (dev)
    Introduces a new struct `RecordComponent` to store associated Record data and AudioClip.
    This struct is intended to be used within the records mechanics feature.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Components/RecordComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using _Project.Scripts.Features.Data.Entities;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Components
+{
+    [Serializable]
+    public struct RecordComponent
+    {
+        public Record Record;
+        public AudioClip AudioClip;
+    }
+}
