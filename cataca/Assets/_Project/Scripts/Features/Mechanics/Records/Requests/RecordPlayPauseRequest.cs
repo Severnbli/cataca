054134a5189ed0866218c021da25a798a67b2b0b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 19:53:52 +0300

    feat: Add RecordPlayPauseRequest struct (dev)
    Introduces a new struct `RecordPlayPauseRequest` to handle
    requests for toggling record play/pause states. This struct
    contains a `Record` object.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Requests/RecordPlayPauseRequest.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Data.Entities;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Requests
+{
+    public struct RecordPlayPauseRequest
+    {
+        public Record Record;
+    }
+}
