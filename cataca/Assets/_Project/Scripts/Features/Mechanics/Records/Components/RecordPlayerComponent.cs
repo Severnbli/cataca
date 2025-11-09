19e2168b1c67fceeea59c7963e4c021331199fc8
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 18:44:00 +0300

    feat: Add RecordPlayerComponent and Converter (dev)
    Introduces the RecordPlayerComponent struct and its corresponding converter.
    This component will be used to manage audio playback for records.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Components/RecordPlayerComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Components
+{
+    [Serializable]
+    public struct RecordPlayerComponent
+    {
+        public AudioSource AudioSource;
+    }
+}
---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Converters/RecordPlayerConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.Mechanics.Records.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Converters
+{
+    public class RecordPlayerConverter : ComponentConverter<RecordPlayerComponent>
+    {
+        
+    }
+}
