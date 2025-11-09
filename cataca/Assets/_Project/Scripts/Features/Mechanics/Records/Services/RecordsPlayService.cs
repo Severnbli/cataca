3b69b4af63bd25312f035bc16d0bd9d60307d344
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 20:14:44 +0300

    feat(records): Add RecordsPlayService (#dev)
    Introduces the RecordsPlayService class.
    This service holds the current record and
    a boolean flag to indicate if playback
    is active.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Services/RecordsPlayService.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.Data.Entities;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Services
+{
+    public sealed class RecordsPlayService
+    {
+        public Record Current;
+        public bool Playing;
+    }
+}
