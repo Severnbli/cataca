f8fbe8c9487c3b1b745e94210a13d35076a2033a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 18:41:55 +0300

    feat: Add storage utilities for levels and records (dev)
    Introduces new methods to `StorageUtils` for loading and saving
    level and record data. This includes generic helper methods
    `LoadBuiltInList` and `SaveBuiltInList` that utilize `PlayerPrefs`
    and `JsonUtils` for serialization and deserialization.

--- cataca/Assets/_Project/Scripts/_Shared/Utils/StorageUtils.cs
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/StorageUtils.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using _Project.Scripts.Features.Data.Entities;
 using _Project.Scripts.Features.Data.Storages.BuiltIn.Configs;
+using UnityEngine;
 
 namespace _Project.Scripts._Shared.Utils
 {
@@ -8,7 +10,36 @@ namespace _Project.Scripts._Shared.Utils
     {
         public static List<Level> LoadLevels(BuiltInStorageConfig config)
         {
-            return new List<Level>();
+            return LoadBuiltInList<Level>(config.Levels);
+        }
+
+        public static void SaveLevels(BuiltInStorageConfig config, List<Level> levels)
+        {
+            SaveBuiltInList(config.Levels, levels);
+        }
+
+        public static List<Record> LoadRecords(BuiltInStorageConfig config)
+        {
+            return LoadBuiltInList<Record>(config.Records);
+        }
+
+        public static void SaveRecords(BuiltInStorageConfig config, List<Record> records)
+        {
+            SaveBuiltInList(config.Records, records);
+        }
+
+        public static List<T> LoadBuiltInList<T>(string path)
+        {
+            var jsonData = PlayerPrefs.GetString(path, string.Empty);
+            var list = string.IsNullOrEmpty(jsonData)
+                ? new List<T>()
+                : JsonUtils.FromJson<List<T>>(jsonData);
+            return list;
+        }
+
+        public static void SaveBuiltInList<T>(string path, List<T> list)
+        {
+            PlayerPrefs.SetString(path, JsonUtils.ToJson(list));
         }
     }
 }
