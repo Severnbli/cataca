0bd3d1a0ecbc1925d8b1e00b63aa0b3078eeb586
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 20:34:16 +0300

    feat(Record): Add equality operators and overrides (#dev)
    Add `operator ==`, `operator !=`, `Equals`, and `GetHashCode` to Record.

--- cataca/Assets/_Project/Scripts/Features/Data/Entities/Record.cs
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/Record.cs
@@ -6,5 +6,32 @@ namespace _Project.Scripts.Features.Data.Entities
     public class Record
     {
         public int Id;
+
+        public static bool operator ==(Record a, Record b)
+        {
+            return b != null && a != null && a.Id == b.Id;
+        }
+
+        public static bool operator !=(Record a, Record b)
+        {
+            return !(a == b);
+        }
+        
+        protected bool Equals(Record other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Record)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() * 47;
+        }
     }
 }
