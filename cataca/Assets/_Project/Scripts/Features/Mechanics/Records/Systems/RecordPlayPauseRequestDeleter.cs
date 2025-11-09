fb63ddd790f1b7be74c6d508ff5bb12ee2d3907e
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-09 20:03:42 +0300

    feat: Add RecordPlayPauseRequestDeleter system (dev)
    Introduces a new system responsible for deleting RecordPlayPauseRequest
    entities. This system is registered in the MenuSceneInstaller to ensure
    it's active when the menu scene loads.
    
    The RecordPlayPauseRequestDeleter system initializes by filtering for
    RecordPlayPauseRequest entities and obtains a pool for them. In its run
    method, it iterates through the filtered entities and deletes each
    RecordPlayPauseRequest.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Systems/RecordPlayPauseRequestDeleter.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Records.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Systems
+{
+    public class RecordPlayPauseRequestDeleter : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<RecordPlayPauseRequest> _recordPlayPauseRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<RecordPlayPauseRequest>()
+                .End();
+            
+            _recordPlayPauseRequestPool = world.GetPool<RecordPlayPauseRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _recordPlayPauseRequestPool.Del(e);
+        }
+    }
+}
