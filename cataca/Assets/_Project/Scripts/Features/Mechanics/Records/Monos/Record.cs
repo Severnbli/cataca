acc60e5252c78832027bd75a8d78b90aedd17df4
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 01:05:19 +0300

    feat(records): Add Record MonoBehaviour script (#dev)
    Added Record class with Icon field and property.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Monos/Record.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Monos
+{
+    public class Record : MonoBehaviour
+    {
+        [SerializeField] private Image _icon;
+        
+        public Image Icon => _icon;
+    }
+}
