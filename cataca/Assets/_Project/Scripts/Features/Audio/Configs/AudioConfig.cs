f46da3faa275b10ec77d1aeb3f4641e38c7b8b98
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 00:42:47 +0300

    feat(audio): Add AudioConfig ScriptableObject (#dev)
    Created AudioConfig ScriptableObject with PlayableIsOnSprite and PlayableIsOffSprite fields.

---
+++ cataca/Assets/_Project/Scripts/Features/Audio/Configs/AudioConfig.cs
@@ -0,0 +1,14 @@
+﻿using _Project.Scripts._Shared.ScriptableObjects;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Audio.Configs
+{
+    public class AudioConfig : ScriptableObjectAutoInstaller<AudioConfig>
+    {
+        [SerializeField] private Sprite _playableIsOnSprite;
+        [SerializeField] private Sprite _playableIsOffSprite;
+        
+        public Sprite PlayableIsOnSprite => _playableIsOnSprite;
+        public Sprite PlayableIsOffSprite => _playableIsOffSprite;
+    }
+}
