bfa5d40ea7238900014866b58224f5f66839139c
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 01:02:18 +0300

    feat(audio): Add public accessor for Playable image (#dev)
    Added public property `Playable` to expose the `_playable` field.

--- cataca/Assets/_Project/Scripts/Features/Audio/Monos/AudioPlayable.cs
+++ cataca/Assets/_Project/Scripts/Features/Audio/Monos/AudioPlayable.cs
@@ -6,5 +6,7 @@ namespace _Project.Scripts.Features.Audio.Monos
     public class AudioPlayable : MonoBehaviour
     {
         [SerializeField] private Image _playable;
+        
+        public Image Playable => _playable;
     }
 }
