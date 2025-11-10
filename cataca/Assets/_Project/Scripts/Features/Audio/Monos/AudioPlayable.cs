4cc070b4864163adfd37b8f38f4e3d7f499e1e75
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 00:54:30 +0300

    feat(audio): Add AudioPlayable mono (#dev)
    Added AudioPlayable MonoBehaviour with PlayableIsOn and PlayableIsOff image fields.

---
+++ cataca/Assets/_Project/Scripts/Features/Audio/Monos/AudioPlayable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.Features.Audio.Monos
+{
+    public class AudioPlayable : MonoBehaviour
+    {
+        [SerializeField] private Image _playableIsOn;
+        [SerializeField] private Image _playableIsOff;
+
+        public Image PlayableIsOn => _playableIsOn;
+        public Image PlayableIsOff => _playableIsOff;
+    }
+}
