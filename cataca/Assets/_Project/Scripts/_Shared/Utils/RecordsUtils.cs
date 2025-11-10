61e9277cefbe1f6f9f90ad1e128b7418edc0d833
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-10 23:49:06 +0300

    feat(utils): Add RecordsUtils class with TryGetAudioClipByRecord (#dev)
    Adds RecordsUtils static class.
    Includes TryGetAudioClipByRecord method to find AudioClip
    by Record ID from a list of RecordComponent.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/RecordsUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _Project.Scripts.Features.Data.Entities;
+using _Project.Scripts.Features.Mechanics.Records.Components;
+using UnityEngine;
+
+namespace _Project.Scripts._Shared.Utils
+{
+    public static class RecordsUtils
+    {
+        public static bool TryGetAudioClipByRecord(List<RecordComponent> records, Record record,
+            out AudioClip audioClip)
+        {
+            audioClip = null;
+            
+            var recordComponentsWithSuchIds = records
+                .Where(x => x.Record.Id == record.Id)
+                .ToList();
+            
+            if (recordComponentsWithSuchIds.Count == 0) return false;
+            
+            var recordComponent = recordComponentsWithSuchIds.First();
+            audioClip = recordComponent.AudioClip;
+            return audioClip != null;
+        }
+    }
+}
