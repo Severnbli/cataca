74c3bc42b037be58b28d31866f913b73771c99f2
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 11:48:42 +0300

    feat(records): Add RecordsInstantiatorConfig ScriptableObject (#dev)
    Added _prefab and _icons fields.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Configs/RecordsInstantiatorConfig.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts._Shared.ScriptableObjects;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Configs
+{
+    public class RecordsInstantiatorConfig : ScriptableObjectAutoInstaller<RecordsInstantiatorConfig>
+    {
+        [SerializeField] private GameObject _prefab;
+        [SerializeField] private Sprite[] _icons;
+    }
+}
