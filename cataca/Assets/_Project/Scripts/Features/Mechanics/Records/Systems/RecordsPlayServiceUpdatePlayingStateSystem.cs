7afb174006b535cfc4579b18b46f0b07766b3400
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 14:58:34 +0300

    feat(records): Add RecordsPlayServiceUpdatePlayingStateSystem (#dev)
    Added RecordsPlayServiceUpdatePlayingStateSystem to manage playing state.
    Handles audio source checks and resets service when playback stops.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Systems/RecordsPlayServiceUpdatePlayingStateSystem.cs
@@ -0,0 +1,44 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features._Shared.Components;
+using _Project.Scripts.Features.Mechanics.Records.Markers;
+using _Project.Scripts.Features.Mechanics.Records.Services;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Systems
+{
+    public class RecordsPlayServiceUpdatePlayingStateSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        public RecordsPlayServiceUpdatePlayingStateSystem(RecordsPlayService recordsPlayService)
+        {
+            _recordsPlayService = recordsPlayService;
+        }
+        
+        private RecordsPlayService _recordsPlayService;
+        private EcsFilter _filter;
+        private EcsPool<AudioSourceComponent> _audioSourcePool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<AudioSourceComponent>()
+                .Inc<RecordsPlayerMarker>()
+                .End();
+            
+            _audioSourcePool = world.GetPool<AudioSourceComponent>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var audioSource = ref _audioSourcePool.Get(e);
+                if (audioSource.AudioSource.isPlaying) return;
+            }
+
+            _recordsPlayService.Playing = false;
+            _recordsPlayService.Current = null;
+        }
+    }
+}
