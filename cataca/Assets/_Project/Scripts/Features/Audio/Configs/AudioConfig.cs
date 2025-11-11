44634677ec17a376a9c2e91023c7a163cdfcf090
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 15:07:17 +0300

    feat(audio): Add mixer groups to AudioConfig (#dev)
    Added GlobalMixerGroup, MusicMixerGroup, and SoundsMixerGroup fields.

--- cataca/Assets/_Project/Scripts/Features/Audio/Configs/AudioConfig.cs
+++ cataca/Assets/_Project/Scripts/Features/Audio/Configs/AudioConfig.cs
@@ -1,5 +1,6 @@
 ﻿using _Project.Scripts._Shared.ScriptableObjects;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace _Project.Scripts.Features.Audio.Configs
 {
@@ -7,8 +8,14 @@ namespace _Project.Scripts.Features.Audio.Configs
     {
         [SerializeField] private Sprite _playableIsOn;
         [SerializeField] private Sprite _playableIsOff;
+        [SerializeField] private AudioMixerGroup _globalMixerGroup;
+        [SerializeField] private AudioMixerGroup _musicMixerGroup;
+        [SerializeField] private AudioMixerGroup _soundsMixerGroup;
         
         public Sprite PlayableIsOn => _playableIsOn;
         public Sprite PlayableIsOff => _playableIsOff;
+        public AudioMixerGroup GlobalMixerGroup => _globalMixerGroup;
+        public AudioMixerGroup MusicMixerGroup => _musicMixerGroup;
+        public AudioMixerGroup SoundsMixerGroup => _soundsMixerGroup;
     }
 }
