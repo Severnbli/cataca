f245473d2d4d2207f3f3e75282430d99982f3c4f
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 15:03:51 +0300

    feat(entities): Add Settings class to Data Entities (#dev)
    Created new Settings class with MusicVolume and SoundVolume properties.

---
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _Project.Scripts.Features.Data.Entities
+{
+    [Serializable]
+    public class Settings
+    {
+        public float MusicVolume { get; set; }
+        public float SoundVolume { get; set; }
+    }
+}
