e3f8cac04a2b1534461d343244152dc0f80a8daf
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 16:13:43 +0300

    fix(settings): Reset volume default values to zero (#dev)
    Changed GlobalVolume, MusicVolume, and SoundsVolume default values from 20f to 0f.

--- cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
@@ -5,8 +5,8 @@ namespace _Project.Scripts.Features.Data.Entities
     [Serializable]
     public class Settings
     {
-        public float GlobalVolume { get; set; } = 20f;
-        public float MusicVolume { get; set; } = 20f;
-        public float SoundsVolume { get; set; } = 20f;
+        public float GlobalVolume { get; set; } = 0f;
+        public float MusicVolume { get; set; } = 0f;
+        public float SoundsVolume { get; set; } = 0f;
     }
 }
