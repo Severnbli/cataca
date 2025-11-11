dd2a466dba5020d5571aff57aa76e6a8b833ef81
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 12:19:42 +0300

    feat(RecordsUtils): Add methods for retrieving RecordComponents (#dev)
    Added GetRecordComponentsByRecords and GetRecordComponentsByMemory
    methods to filter RecordComponents based on provided records or
    memory configuration.

--- cataca/Assets/_Project/Scripts/_Shared/Utils/RecordsUtils.cs
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/RecordsUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using _Project.Scripts.Features.Data.Entities;
+using _Project.Scripts.Features.Data.Storages.BuiltIn.Configs;
 using _Project.Scripts.Features.Mechanics.Records.Components;
 using UnityEngine;
 
@@ -22,6 +23,26 @@ namespace _Project.Scripts._Shared.Utils
             var recordComponent = recordComponentsWithSuchIds.First();
             audioClip = recordComponent.AudioClip;
             return audioClip != null;
+        }
+
+        public static List<RecordComponent> GetRecordComponentsByRecords(List<RecordComponent> recordComponents,
+            List<Record> records)
+        {
+            var ids = records.Select(x => x.Id).ToList();
+            
+            var result = recordComponents
+                .Where(x => ids.Contains(x.Record.Id))
+                .ToList();
+            
+            return result;
+        }
+
+        public static List<RecordComponent> GetRecordComponentsByMemory(List<RecordComponent> recordComponents,
+            BuiltInStorageConfig config)
+        {
+            var records = StorageUtils.LoadRecords(config);
+            
+            return GetRecordComponentsByRecords(recordComponents, records);
         }
     }
 }
