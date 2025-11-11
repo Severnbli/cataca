d0e669a3f4fba8387da4a83552484b886c0fef5b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 15:49:00 +0300

    feat(settings): Set default audio volume values (#dev)
    Initialized GlobalVolume, MusicVolume, and SoundsVolume properties
    with default values of 20f.

--- cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
@@ -5,8 +5,8 @@ namespace _Project.Scripts.Features.Data.Entities
     [Serializable]
     public class Settings
     {
-        public float GlobalVolume { get; set; }
-        public float MusicVolume { get; set; }
-        public float SoundsVolume { get; set; }
+        public float GlobalVolume { get; set; } = 20f;
+        public float MusicVolume { get; set; } = 20f;
+        public float SoundsVolume { get; set; } = 20f;
     }
 }
