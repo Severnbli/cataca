a72dbc3a1bf1ca54b96d9e6b25da03b6e26b096d
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 16:31:34 +0300

    feat(audio): Add AudioUtils static class (#dev)
    Implemented LinearToDecibels and DecibelsToLinear conversion methods.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/AudioUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts._Shared.Utils
+{
+    public static class AudioUtils
+    {
+        public static float LinearToDecibels(float linear)
+        {
+            linear = Mathf.Clamp(linear, 0f, 1f);
+            return Mathf.Log10(linear) * 20f;
+        }
+
+        public static float DecibelsToLinear(float decibel) {
+            var value = Mathf.Pow(10f, decibel / 20f);
+            return Mathf.Clamp(value, 0f, 1f);
+        }
+    }
+}
