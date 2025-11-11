e08c266dd551ad49f36824c884955a69cf5a2262
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 11:53:28 +0300

    feat(records): Change RecordsContainerMarker to struct (#dev)

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Converters/RecordsContainerMarkerConverter.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Converters/RecordsContainerMarkerConverter.cs
@@ -1,6 +1,10 @@
-﻿namespace _Project.Scripts.Features.Mechanics.Records.Converters
+﻿using System.ComponentModel;
+using _Project.Scripts.Features.Mechanics.Records.Markers;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Converters
 {
-    public class RecordsContainerMarkerConverter
+    public class RecordsContainerMarkerConverter : ComponentConverter<RecordsContainerMarker>
     {
         
     }
