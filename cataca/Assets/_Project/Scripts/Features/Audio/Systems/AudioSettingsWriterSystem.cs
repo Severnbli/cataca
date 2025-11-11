3dbee9379f190083c421f8790313e685a48c5825
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 16:43:31 +0300

    feat(audio): Add AudioSettingsWriterSystem (#dev)
    Implements system for saving audio settings.
    Writes current volume levels to storage on destroy.

---
+++ cataca/Assets/_Project/Scripts/Features/Audio/Systems/AudioSettingsWriterSystem.cs
@@ -0,0 +1,30 @@
+﻿using _Project.Scripts._Shared.Utils;
+using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Audio.Services;
+using _Project.Scripts.Features.Data.Storages.BuiltIn.Configs;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Audio.Systems
+{
+    public class AudioSettingsWriterSystem : IEcsDestroySystem, IEcsGameSystem
+    {
+        public AudioSettingsWriterSystem(AudioService audioService)
+        {
+            _audioService = audioService;
+        }
+        
+        private AudioService _audioService;
+        private BuiltInStorageConfig _storageConfig;
+        
+        public void Destroy(IEcsSystems systems)
+        {
+            var settings = StorageUtils.LoadSettings(_storageConfig);
+            
+            settings.GlobalVolume = _audioService.CurrentGlobalVolume;
+            settings.MusicVolume = _audioService.CurrentMusicVolume;
+            settings.SoundsVolume = _audioService.CurrentSoundsVolume;
+            
+            StorageUtils.SaveSettings(_storageConfig, settings);
+        }
+    }
+}
