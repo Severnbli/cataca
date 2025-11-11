c86c5732c41ecf3f89d0c8661a8f6128ca60d1ec
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-11 15:42:54 +0300

    feat(settings): Add GlobalVolume to Settings class (#dev)

--- cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/Settings.cs
@@ -5,6 +5,7 @@ namespace _Project.Scripts.Features.Data.Entities
     [Serializable]
     public class Settings
     {
+        public float GlobalVolume { get; set; }
         public float MusicVolume { get; set; }
         public float SoundsVolume { get; set; }
     }
