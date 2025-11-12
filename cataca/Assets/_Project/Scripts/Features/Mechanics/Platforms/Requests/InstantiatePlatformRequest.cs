0eed4aea98faeec3ba552163a72ace419c5d78b4
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 00:00:19 +0300

    feat(platforms): Add InstantiatePlatformRequest struct (#dev)

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Requests/InstantiatePlatformRequest.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Requests
+{
+    public struct InstantiatePlatformRequest
+    {
+        public GameObject Platform;
+    }
+}
