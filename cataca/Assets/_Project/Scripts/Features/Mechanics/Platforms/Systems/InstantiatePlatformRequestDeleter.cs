514fa5f41adc86480c69ba50c2b48c1d6b28782b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 00:00:25 +0300

    feat(platforms): Add InstantiatePlatformRequestDeleter system (#dev)
    Added InstantiatePlatformRequestDeleter system to ECS.
    This system deletes InstantiatePlatformRequest entities after processing.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/InstantiatePlatformRequestDeleter.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class InstantiatePlatformRequestDeleter : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<InstantiatePlatformRequest> _instantiatePlatformRequestsPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<InstantiatePlatformRequest>()
+                .End();
+            
+            _instantiatePlatformRequestsPool = world.GetPool<InstantiatePlatformRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _instantiatePlatformRequestsPool.Del(e);
+        }
+    }
+}
