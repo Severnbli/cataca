e9edf4fd3e1feff805ebba798ea45fca77efc8ab
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 01:41:55 +0300

    feat(platforms): Add PlatformConverter for component conversion (#dev)
    Added PlatformConverter class inheriting from ComponentConverter.
    Added [Serializable] attribute to PlatformComponent struct.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Components/PlatformComponent.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Components/PlatformComponent.cs
@@ -3,6 +3,7 @@ using _Project.Scripts.Features.Mechanics.Platforms.Monos;
 
 namespace _Project.Scripts.Features.Mechanics.Platforms.Components
 {
+    [Serializable]
     public struct PlatformComponent
     {
         public Platform Platform;
