611741ae63971708ac7fef2f1050c6344c2a2670
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 01:42:06 +0300

    feat(platforms): Add PositionPlatformMarkerConverter (#dev)
    Added PositionPlatformMarkerConverter.cs and PositionPlatformMarker.cs.
    Marked PositionPlatformMarker as Serializable.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Converters/PositionPlatformMarkerConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.Mechanics.Platforms.Markers;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Converters
+{
+    public class PositionPlatformMarkerConverter : ComponentConverter<PositionPlatformMarker>
+    {
+        
+    }
+}
