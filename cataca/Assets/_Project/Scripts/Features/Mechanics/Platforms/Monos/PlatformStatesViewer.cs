38fcddc06fbf2fd3b372f691b2d0b868b07a1aeb
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-12 21:14:52 +0300

    feat(platforms): Add PlatformStatesViewer script (#dev)
    Implements a new MonoBehaviour script for visualizing platform states in the Unity editor. Includes Gizmos and Handles drawing logic for platform colliders and state transforms.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Monos/PlatformStatesViewer.cs
@@ -0,0 +1,32 @@
+﻿using _Project.Scripts._Shared.Utils;
+
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEngine;
+#endif
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Monos
+{
+    public class PlatformStatesViewer : MonoBehaviour
+    {
+        [SerializeField] private Platform _platform;
+
+#if UNITY_EDITOR
+        [SerializeField] private Color _gizmosColor = Color.green;
+        [SerializeField] private Color _handlesColor = Color.cyan;
+        
+        private void OnDrawGizmos()
+        {
+            if (_platform == null || _platform.States == null || _platform.Collider == null) return;
+
+            var nonNullStates = _platform.States.Where(x => x != null).ToList();
+            
+            for (var i = 0; i < nonNullStates.Count; i++)
+            {
+                Collider2DUtils.DrawColliderAt(_platform.Collider, nonNullStates[i].transform, _gizmosColor);
+                EditorUtils.DrawHandle(nonNullStates[i].transform.position, i.ToString(), _handlesColor);
+            }
+        }
+#endif
+    }
+}
