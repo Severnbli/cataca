5836961b572a6624e32db586b1e7274f1a934ce9
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-12 23:43:09 +0300

    fix(EditorUtils): Add ValidateResponse method (#dev)
    Added ValidateResponse method to EditorUtils.cs.
    Included System, System.Collections.Generic, and System.Linq namespaces.

--- cataca/Assets/_Project/Scripts/_Shared/Utils/EditorUtils.cs
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/EditorUtils.cs
@@ -1,4 +1,7 @@
 ﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
@@ -27,6 +30,27 @@ namespace _Project.Scripts._Shared.Utils
                     fontStyle = FontStyle.Bold,
                     alignment = TextAnchor.MiddleCenter
                 });
+        }
+
+        public static void ValidateResponse(HashSet<KeyValuePair<Func<bool>, string>> checks, string name)
+        {
+            var output = new List<string>(checks.Count);
+            
+            foreach (var check in checks)
+            {
+                if (!check.Key.Invoke()) output.Add(check.Value);
+            }
+
+            if (!output.Any())
+            {
+                Debug.Log($"Validate {name} succeeded.");
+                return;
+            }
+            
+            var finalOutput = output
+                .Select((txt, i) => $"{i + 1}) {txt}")
+                .ToList();
+            Debug.LogWarning($"Validation {name} failed: {string.Join(";\n", finalOutput)}");
         }
     }
 }
