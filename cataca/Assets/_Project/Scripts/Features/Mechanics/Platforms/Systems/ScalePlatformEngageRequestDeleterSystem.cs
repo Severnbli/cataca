3e920811f8f46d72bb660aea275bccd4a79541eb
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 00:16:48 +0300

    feat(platforms): Add ScalePlatformEngageRequestDeleterSystem (#dev)
    Added ScalePlatformEngageRequestDeleterSystem to remove ScalePlatformEngageRequest entities.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/ScalePlatformEngageRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class ScalePlatformEngageRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<ScalePlatformEngageRequest> _scalePlatformEngageRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<ScalePlatformEngageRequest>()
+                .End();
+            
+            _scalePlatformEngageRequestPool = world.GetPool<ScalePlatformEngageRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _scalePlatformEngageRequestPool.Del(e);
+        }
+    }
+}
