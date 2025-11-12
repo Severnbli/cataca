c676050d421a1ca07394636d6a9425e222c5b778
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-12 21:14:41 +0300

    feat(platforms): Add Platform MonoBehaviour (#dev)
    Implemented Platform script with fields for SpriteRenderer, Collider2D, and a states container. Added a property to retrieve child transforms from the states container.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Monos/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Monos
+{
+    public class Platform : MonoBehaviour
+    {
+        [SerializeField] private GameObject _statesContainer;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Collider2D _collider;
+        
+        public SpriteRenderer SpriteRenderer => _spriteRenderer;
+        public Collider2D Collider => _collider;
+
+        public List<Transform> States
+        {
+            get
+            {
+                var states = _statesContainer != null 
+                    ? _statesContainer.GetComponentsInChildren<Transform>()
+                        .Where(x => x != _statesContainer.transform)
+                        .ToList()
+                    : new List<Transform>();
+                return states;
+            }
+        }
+    }
+}
