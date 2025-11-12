a6d36c664a524ac00b822a1227c9d9945a2b18dd
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-12 21:21:23 +0300

    feat(platforms): Add PlatformComponent struct (#dev)

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Components/PlatformComponent.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Platforms.Monos;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Components
+{
+    public struct PlatformComponent
+    {
+        public Platform Platform;
+    }
+}
