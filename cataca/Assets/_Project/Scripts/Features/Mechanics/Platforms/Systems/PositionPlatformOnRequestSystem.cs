0ce69dbeea4bc10d434ef5c3c33320e145b0ec8d
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 01:16:19 +0300

    feat(platforms): Add PositionPlatformOnRequestSystem (#dev)
    Introduced PositionPlatformOnRequestSystem to handle platform movement requests.
    Initializes ECS filter and pools.
    Updates platform position ID and creates DOTween sequences for movement.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/PositionPlatformOnRequestSystem.cs
@@ -0,0 +1,70 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features._Shared.Components;
+using _Project.Scripts.Features.Mechanics.Platforms.Components;
+using _Project.Scripts.Features.Mechanics.Platforms.Configs;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using DG.Tweening;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class PositionPlatformOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        public PositionPlatformOnRequestSystem(PlatformAnimationConfig animationConfig)
+        {
+            _animationConfig = animationConfig;
+        }
+        
+        private PlatformAnimationConfig _animationConfig;
+        private EcsFilter _filter;
+        private EcsPool<PlatformComponent> _platformPool;
+        private EcsPool<TweenComponent> _tweenPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<PlatformComponent>()
+                .Inc<PositionPlatformRequest>()
+                .End();
+            
+            _platformPool = world.GetPool<PlatformComponent>();
+            _tweenPool = world.GetPool<TweenComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var platform = ref _platformPool.Get(e);
+
+                var states = platform.Platform.States;
+
+                var nextId = platform.PositionId + 1;
+                platform.PositionId = nextId < states.Count
+                    ? nextId
+                    : 0;
+                
+                ref var tween = ref _tweenPool.Has(e)
+                    ? ref _tweenPool.Get(e)
+                    : ref _tweenPool.Add(e);
+                
+                var sequence = DOTween.Sequence();
+
+                if (tween.Tween is not null && tween.Tween.active) sequence.Append(tween.Tween);
+                
+                var targetPosition = states[platform.PositionId].position;
+                
+                sequence.Append(platform.Platform.Object.transform
+                    .DOMove(targetPosition, _animationConfig.Duration)
+                    .SetEase(_animationConfig.Ease)
+                );
+                
+                sequence.AppendInterval(_animationConfig.TransitionDuration);
+                
+                tween.Tween = sequence;
+            }
+        }
+    }
+}
