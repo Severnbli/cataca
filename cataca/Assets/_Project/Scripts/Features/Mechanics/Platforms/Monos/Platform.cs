3e30ad55a4c35094b7965fad8a8945354b5ee50d
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-12 23:52:15 +0300

    feat(platform): Add validation method to Platform mono (#dev)
    Added Validate method with checks for required components and duplicate types.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Monos/Platform.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Monos/Platform.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using _Project.Scripts.Features.Mechanics.Platforms.Enums;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using System;
+using _Project.Scripts._Shared.Utils;
+using Sirenix.OdinInspector;
+#endif
 
 namespace _Project.Scripts.Features.Mechanics.Platforms.Monos
 {
@@ -28,5 +35,21 @@ namespace _Project.Scripts.Features.Mechanics.Platforms.Monos
                 return states;
             }
         }
+        
+#if UNITY_EDITOR
+        [PropertySpace(10)]
+        [Button]
+        private void Validate()
+        {
+            var checks = new Dictionary<Func<bool>, string>();
+
+            checks.TryAdd(() => _statesContainer == null, "StatesContainer not set");
+            checks.TryAdd(() => _spriteRenderer == null, "SpriteRenderer not set");
+            checks.TryAdd(() => _collider == null, "Collider not set");
+            checks.TryAdd(() => _platformTypes.Length != _platformTypes.Distinct().Count(), "Platform types duplicated");
+            
+            EditorUtils.Validate(checks, nameof(Platform));
+        }
+#endif
     }
 }
