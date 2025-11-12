fc9040f16e2d41724017572d2518467d670fcd84
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-12 20:40:01 +0300

    feat(EditorUtils): Add DrawHandle method (#dev)
    Implemented DrawHandle for displaying bold, colored text labels in editor scenes.

--- cataca/Assets/_Project/Scripts/_Shared/Utils/EditorUtils.cs
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/EditorUtils.cs
@@ -1,4 +1,6 @@
 ﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
 
 namespace _Project.Scripts._Shared.Utils
 {
@@ -6,12 +8,25 @@ namespace _Project.Scripts._Shared.Utils
     {
         public static bool Confirm(string msg)
         {
-            return UnityEditor.EditorUtility.DisplayDialog("Confirm Action", msg, "Yes", "Cancel");
+            return EditorUtility.DisplayDialog("Confirm Action", msg, "Yes", "Cancel");
         }
 
         public static void ShowMessage(string msg)
         {
-            UnityEditor.EditorUtility.DisplayDialog("Information", msg, "Ok");
+            EditorUtility.DisplayDialog("Information", msg, "Ok");
+        }
+
+        public static void DrawHandle(Vector3 at, string text, Color color)
+        {
+            Handles.Label(
+                at,
+                new GUIContent(text),
+                new GUIStyle
+                {
+                    normal = { textColor = color },
+                    fontStyle = FontStyle.Bold,
+                    alignment = TextAnchor.MiddleCenter
+                });
         }
     }
 }
