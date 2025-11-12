c5244035c42e0004bc7607c3a8edb4384640c77c
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 00:16:42 +0300

    feat(platforms): Add RotatePlatformEngageRequestDeleterSystem (#dev)
    Created RotatePlatformEngageRequestDeleterSystem to remove RotatePlatformEngageRequest entities.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/RotatePlatformEngageRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class RotatePlatformEngageRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<RotatePlatformEngageRequest> _rotationPlatformEngageRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<RotatePlatformEngageRequest>()
+                .End();
+            
+            _rotationPlatformEngageRequestPool = world.GetPool<RotatePlatformEngageRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _rotationPlatformEngageRequestPool.Del(e);
+        }
+    }
+}
