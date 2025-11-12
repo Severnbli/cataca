6d6e454252cf329f159fbab80b860c0b89ae853b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 00:43:24 +0300

    feat(platforms): Add PlatformAnimationConfig scriptable object (#dev)

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Configs/PlatformAnimationConfig.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts._Shared.ScriptableObjects;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Configs
+{
+    public class PlatformAnimationConfig : ScriptableObjectAutoInstaller<PlatformAnimationConfig>
+    {
+        [SerializeField] private float duration;
+        [SerializeField] private Ease ease = Ease.InOutQuad;
+        
+        public float Duration => duration;
+        public Ease Ease => ease;
+    }
+}
