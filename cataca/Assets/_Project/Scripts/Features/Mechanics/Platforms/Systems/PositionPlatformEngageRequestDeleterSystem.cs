c696d9fc31236499e3acc1855186332ee560b35d
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 00:16:35 +0300

    feat(platforms): Add PositionPlatformEngageRequestDeleterSystem (#dev)
    Added PositionPlatformEngageRequestDeleterSystem to remove engage requests.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/PositionPlatformEngageRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class PositionPlatformEngageRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<PositionPlatformEngageRequest> _positionPlatformEngageRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<PositionPlatformEngageRequest>()
+                .End();
+            
+            _positionPlatformEngageRequestPool = world.GetPool<PositionPlatformEngageRequest>();  
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _positionPlatformEngageRequestPool.Del(e);
+        }
+    }
+}
