43d2afe0352aee1f3545848e37f5e082bb62c606
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 22:17:18 +0300

    feat(extensions): Add GetChildComponents to GameObjectExtensions (#dev)
    Added GetChildComponents method to retrieve components from child GameObjects, excluding the parent's component.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _Project.Scripts._Shared.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static List<T> GetChildComponents<T>(this GameObject gameObject) where T : Component
+        {
+            var ownComponent = gameObject.GetComponent<T>();
+            var components = gameObject != null ? 
+                gameObject.GetComponentsInChildren<T>()
+                    .Where(x => x != ownComponent)
+                    .ToList()
+                : new List<T>();
+            
+            return components;
+        }
+    }
+}
