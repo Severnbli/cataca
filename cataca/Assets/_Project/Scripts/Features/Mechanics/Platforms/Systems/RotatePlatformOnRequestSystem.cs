279bd8b08d5c2367e891c10a8f186fba1b3f3e0e
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-13 17:57:47 +0300

    refactor(tween): Replace SequenceComponent with TweenQueueComponent (#dev)
    Replaced SequenceComponent with TweenQueueComponent for managing DOTween sequences.
    Added TweenQueueUpdateSystem to process the tween queue.
    Updated platform systems to enqueue tweens instead of directly managing sequences.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/RotatePlatformOnRequestSystem.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/RotatePlatformOnRequestSystem.cs
@@ -1,4 +1,6 @@
-﻿using _Project.Scripts.Core.Systems.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using _Project.Scripts.Core.Systems.Interfaces;
 using _Project.Scripts.Features._Shared.Components;
 using _Project.Scripts.Features.Mechanics.Platforms.Components;
 using _Project.Scripts.Features.Mechanics.Platforms.Configs;
@@ -18,7 +20,7 @@ namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
         private PlatformAnimationConfig _animationConfig;
         private EcsFilter _filter;
         private EcsPool<PlatformComponent> _platformPool;
-        private EcsPool<SequenceComponent> _sequencePool;
+        private EcsPool<TweenQueueComponent> _tweenQueuePool;
         
         public void Init(IEcsSystems systems)
         {
@@ -30,7 +32,7 @@ namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
                 .End();
             
             _platformPool = world.GetPool<PlatformComponent>();
-            _sequencePool = world.GetPool<SequenceComponent>();
+            _tweenQueuePool = world.GetPool<TweenQueueComponent>();
         }
 
         public void PostRun(IEcsSystems systems)
@@ -46,20 +48,27 @@ namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
                     ? nextId
                     : 0;
                 
-                ref var sequence = ref _sequencePool.Has(e)
-                    ? ref _sequencePool.Get(e)
-                    : ref _sequencePool.Add(e);
-                
-                if (sequence.Sequence is not { active: true }) sequence.Sequence = DOTween.Sequence();
+                ref var tweenQueue = ref _tweenQueuePool.Has(e)
+                    ? ref _tweenQueuePool.Get(e)
+                    : ref _tweenQueuePool.Add(e);
                 
                 var targetRotation = states[platform.RotateId].rotation;
+                var localPlatform = platform.Platform;
                 
-                sequence.Sequence.Append(platform.Platform.Object.transform
-                    .DORotateQuaternion(targetRotation, _animationConfig.Duration)
-                    .SetEase(_animationConfig.Ease)
-                );
+                tweenQueue.Queue ??= new Queue<Func<Tween>>();
+                tweenQueue.Queue.Enqueue(() =>
+                {
+                    var sequence = DOTween.Sequence();
+
+                    sequence.Append(localPlatform.Object.transform
+                        .DORotateQuaternion(targetRotation, _animationConfig.Duration)
+                        .SetEase(_animationConfig.Ease)
+                    );
                 
-                sequence.Sequence.AppendInterval(_animationConfig.TransitionDuration);
+                    sequence.AppendInterval(_animationConfig.TransitionDuration);
+                    
+                    return sequence;
+                });
             }
         }
     }
---
+++ cataca/Assets/_Project/Scripts/Features/_Shared/Components/TweenQueueComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+
+namespace _Project.Scripts.Features._Shared.Components
+{
+    public struct TweenQueueComponent
+    {
+        public Queue<Func<Tween>> Queue;
+    }
+}
