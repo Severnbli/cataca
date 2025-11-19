11bc557f5900d09eb6812faa4ccd28af986630ff
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-19 20:46:26 +0300

    feat(ShaderUtils): Add utility class for shader property manipulation (#dev)
    Introduces a new static utility class, ShaderUtils, to streamline common operations
    related to shader properties. This class provides methods for:
    
    - Retrieving all property names from a shader.
    - Retrieving all property types from a shader.
    - Grouping property names by their respective types.
    - Filtering property names based on a specific type.
    
    This aims to simplify shader management and inspection within the project.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/ShaderUtils.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace _Project.Scripts._Shared.Utils
+{
+    public static class ShaderUtils
+    {
+        public static List<string> GetPropertiesNames(Shader shader)
+        {
+            var count = shader.GetPropertyCount();
+            var properties = new List<string>(count);
+            
+            for (var i = 0; i < count; i++)
+            {
+                properties.Add(shader.GetPropertyName(i));
+            }
+            
+            return properties;
+        }
+
+        public static List<ShaderPropertyType> GetPropertiesTypes(Shader shader)
+        {
+            var count = shader.GetPropertyCount();
+            var properties = new List<ShaderPropertyType>(count);
+
+            for (var i = 0; i < count; i++)
+            {
+                properties.Add(shader.GetPropertyType(i));
+            }
+            
+            return properties;
+        }
+
+        public static Dictionary<ShaderPropertyType, List<string>> GetPropertiesNamesTypes(Shader shader)
+        {
+            var count = shader.GetPropertyCount();
+            var properties = new Dictionary<ShaderPropertyType, List<string>>(count);
+
+            for (var i = 0; i < count; i++)
+            {
+                var propertyType = shader.GetPropertyType(i);
+                if (!properties.TryGetValue(propertyType, out var names))
+                {
+                    names = new List<string>();
+                    if (!properties.TryAdd(propertyType, names)) continue;
+                }
+                
+                names.Add(shader.GetPropertyName(i));
+            }
+            
+            return properties;
+        }
+
+        public static List<string> GetPropertiesNamesByType(Dictionary<ShaderPropertyType, List<string>> properties,
+            ShaderPropertyType type)
+        {
+            if (!properties.TryGetValue(type, out var names))
+            {
+                names = new List<string>();
+            }
+            
+            return names;
+        }
+    }
+}
