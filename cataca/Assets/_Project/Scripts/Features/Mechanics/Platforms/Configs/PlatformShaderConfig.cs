5f32d65053019aea235981028bdc97f9ec63dabe
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-19 21:27:00 +0300

    feat(Platforms): Add PlatformShaderConfig ScriptableObject (#dev)
    Introduces a new ScriptableObject, `PlatformShaderConfig`, designed to manage shader properties for platform mechanics.
    
    This configuration allows developers to:
    - Assign a specific shader.
    - Map shader properties for color and enablement states related to position, rotation, and scaling.
    - Configure default color values for these states.
    
    The implementation includes editor-specific logic to dynamically retrieve shader property names and populate dropdowns, enhancing usability and reducing manual configuration errors. It also provides utility methods for shader property inspection.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Configs/PlatformShaderConfig.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts._Shared.ScriptableObjects;
+using _Project.Scripts._Shared.Utils;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Configs
+{
+    public class PlatformShaderConfig : ScriptableObjectAutoInstaller<PlatformShaderConfig>
+    {
+        [OnValueChanged(nameof(UpdateParameters))]
+        [SerializeField] 
+        private Shader _shader;
+
+        [ShowIf(nameof(HasShader))] 
+        [SerializeField]
+        [ValueDropdown(nameof(GetColorProperties))]
+        private string _positionColorProp;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        [ValueDropdown(nameof(GetFloatProperties))]
+        private string _positionEnabledProp;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        private Color _positionColor;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        [ValueDropdown(nameof(GetColorProperties))]
+        private string _rotateColorProp;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        [ValueDropdown(nameof(GetFloatProperties))]
+        private string _rotateEnabledProp;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        private Color _rotateColor;
+
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        [ValueDropdown(nameof(GetColorProperties))]
+        private string _scaleColorProp;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        [ValueDropdown(nameof(GetFloatProperties))]
+        private string _scaleEnabledProp;
+        
+        [ShowIf(nameof(HasShader))]
+        [SerializeField]
+        private Color _scaleColor;
+
+#if UNITY_EDITOR
+        private Dictionary<ShaderPropertyType, List<string>> _shaderProperties = new();
+
+        private bool HasShader => _shader != null;
+
+        private List<string> GetColorProperties =>
+            ShaderUtils.GetPropertiesNamesByType(_shaderProperties, ShaderPropertyType.Color);
+        
+        private List<string> GetFloatProperties => ShaderUtils.GetPropertiesNamesByType(_shaderProperties, 
+            ShaderPropertyType.Float);
+
+        [PropertySpace(10)]
+        [Button]
+        private void UpdateParameters()
+        {
+            _shaderProperties.Clear();
+
+            if (!HasShader)
+            {
+                ResetParameters();
+                return;
+            }
+            
+            _shaderProperties = ShaderUtils.GetPropertiesNamesTypes(_shader);
+        }
+
+        private void ResetParameters()
+        {
+            _positionColorProp = string.Empty;
+            _positionEnabledProp = string.Empty;
+            
+            _rotateColorProp = string.Empty;
+            _rotateEnabledProp = string.Empty;
+            
+            _scaleColorProp = string.Empty;
+            _scaleEnabledProp = string.Empty;
+        }
+
+        [PropertySpace(10)]
+        [Button]
+        private void Validate()
+        {
+            
+        }
+#endif
+    }
+}
