fb9f9b609caad0ed19495cc475097bc2afdf9b8a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-21 00:35:46 +0300

    feat(extensions): add SpriteGlowEffect DOTween extensions (#dev)
    Introduces new extension methods for the SpriteGlowEffect component to allow
    for smooth animation of its glow color and brightness using DOTween.
    
    This feature facilitates:
    - Animating the glow color over time.
    - Animating the glow brightness over time.
    - Seamless integration with existing DOTween animation workflows.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Extensions/SpriteGlowExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using SpriteGlow;
+using UnityEngine;
+
+namespace _Project.Scripts._Shared.Extensions
+{
+    public static class SpriteGlowExtensions
+    {
+        public static TweenerCore<Color, Color, ColorOptions> DOColor(this SpriteGlowEffect target, Color endValue,
+            float duration)
+        {
+            var t = DOTween.To(() => target.GlowColor, x => target.GlowColor = x, endValue, duration);
+            t.SetTarget(target);
+            return t;
+        }
+
+        public static TweenerCore<float, float, FloatOptions> DOBrightness(this SpriteGlowEffect target, float endValue,
+            float duration)
+        {
+            var t = DOTween.To(() => target.GlowBrightness, x => target.GlowBrightness = x, endValue,  duration);
+            t.SetTarget(target);
+            return t;
+        }
+    }
+}
