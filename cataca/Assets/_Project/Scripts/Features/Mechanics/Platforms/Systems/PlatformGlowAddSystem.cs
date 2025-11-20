5d5dcb2a458203744a8ebbe09833d3f35a45f389
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-21 02:14:59 +0300

    feat(platforms): add PlatformGlowAddSystem (#dev)
    Introduces a new ECS system responsible for managing platform glow effects.
    This system initializes `PlatformGlowComponent` and associated tween
    components for entities that have a `PlatformComponent` but not yet
    a `PlatformGlowComponent`. This allows for dynamic addition of glow
    visuals to platforms.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/PlatformGlowAddSystem.cs
@@ -0,0 +1,42 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Anims.Components;
+using _Project.Scripts.Features.Mechanics.Platforms.Components;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class PlatformGlowAddSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsWorld _world;
+        private EcsFilter _filter;
+        private EcsPool<PlatformGlowComponent> _platformGlowPool;
+        private EcsPool<TweenComponent> _tweenPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            _world = systems.GetWorld();
+
+            _filter = _world
+                .Filter<PlatformComponent>()
+                .Exc<PlatformGlowComponent>()
+                .End();
+            
+            _platformGlowPool = _world.GetPool<PlatformGlowComponent>();
+            _tweenPool = _world.GetPool<TweenComponent>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var platformGlow = ref _platformGlowPool.Add(e);
+                
+                platformGlow.PulseAnimEntity = _world.NewEntity();
+                _tweenPool.Add(platformGlow.PulseAnimEntity);
+                
+                platformGlow.TransitionAnimEntity = _world.NewEntity();
+                _tweenPool.Add(platformGlow.TransitionAnimEntity);
+            }
+        }
+    }
+}
