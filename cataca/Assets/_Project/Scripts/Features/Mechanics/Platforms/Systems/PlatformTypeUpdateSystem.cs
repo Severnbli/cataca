b6a02fc308a98d679ac2d0fb12e7ce534590cab6
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-20 22:41:57 +0300

    feat(platforms): add PlatformTypeUpdateSystem (#dev)
    Introduces a new ECS system, PlatformTypeUpdateSystem, responsible for managing
    platform type markers. This system iterates through all entities with a
    PlatformComponent and adds or removes corresponding marker components
    (PositionPlatformMarker, RotatePlatformMarker, ScalePlatformMarker) based on
    the presence of specific PlatformTypes in the PlatformComponent. This ensures
    that entities are correctly tagged for different platform behaviors.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/PlatformTypeUpdateSystem.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Components;
+using _Project.Scripts.Features.Mechanics.Platforms.Enums;
+using _Project.Scripts.Features.Mechanics.Platforms.Markers;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class PlatformTypeUpdateSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<PlatformComponent> _platformPool;
+        private EcsPool<PositionPlatformMarker> _positionPlatformMarkerPool;
+        private EcsPool<RotatePlatformMarker> _rotatePlatformMarkerPool;
+        private EcsPool<ScalePlatformMarker> _scalePlatformMarkerPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<PlatformComponent>()
+                .End();
+            
+            _platformPool = world.GetPool<PlatformComponent>();
+            _positionPlatformMarkerPool = world.GetPool<PositionPlatformMarker>();
+            _rotatePlatformMarkerPool = world.GetPool<RotatePlatformMarker>();
+            _scalePlatformMarkerPool = world.GetPool<ScalePlatformMarker>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var platform = ref _platformPool.Get(e);
+
+                if (platform.Platform.PlatformTypes.Contains(PlatformType.Position))
+                {
+                    _positionPlatformMarkerPool.Add(e);
+                }
+                else
+                {
+                    _positionPlatformMarkerPool.Del(e);
+                }
+
+                if (platform.Platform.PlatformTypes.Contains(PlatformType.Rotate))
+                {
+                    _rotatePlatformMarkerPool.Add(e);
+                }
+                else
+                {
+                    _rotatePlatformMarkerPool.Del(e);
+                }
+
+                if (platform.Platform.PlatformTypes.Contains(PlatformType.Scale))
+                {
+                    _scalePlatformMarkerPool.Add(e);
+                }
+                else
+                {
+                    _scalePlatformMarkerPool.Del(e);
+                }
+            }
+        }
+    }
+}
