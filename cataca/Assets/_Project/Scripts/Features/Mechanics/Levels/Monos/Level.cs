610a4c81eb4c41c07040eaea7c447afcf8b65b39
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-21 11:40:22 +0300

    feat(levels): add Level MonoBehaviour script (#)

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Monos
+{
+    public class Level : MonoBehaviour
+    {
+        [SerializeField] private GameObject _platformsParent;
+        
+        public GameObject PlatformsParent => _platformsParent;
+    }
+}
