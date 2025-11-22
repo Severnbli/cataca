ad5e643225fb74904dcb4aca7cc1899c2121b251
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 22:01:30 +0300

    feat(levels): add system to send destroy request (#dev)
    Introduces a new ECS system, `SendDestroyLoadedLevelRequestOnInstantiateLevelRequestSystem`.
    This system is responsible for automatically sending a `DestroyLoadedLevelRequest`
    whenever an `InstantiateLevelRequest` is processed. This ensures that previously
    loaded levels are properly cleaned up before a new one is instantiated, preventing
    potential resource leaks or conflicts.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/SendDestroyLoadedLevelRequestOnInstantiateLevelRequestSystem.cs
@@ -0,0 +1,35 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class SendDestroyLoadedLevelRequestOnInstantiateLevelRequestSystem : IEcsInitSystem, IEcsPostRunSystem, 
+        IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<DestroyLoadedLevelRequest> _destroyLoadedLevelRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<InstantiateLevelRequest>()
+                .End();
+            
+            _destroyLoadedLevelRequestPool = world.GetPool<DestroyLoadedLevelRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                if (!_destroyLoadedLevelRequestPool.Has(e))
+                {
+                    _destroyLoadedLevelRequestPool.Add(e);
+                }
+            }
+        }
+    }
+}
