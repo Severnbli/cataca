1ab975cd88c5f778e2b51738d5676dcb63e1ed74
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 21:45:10 +0300

    feat(levels): add system to instantiate levels on request (#dev)
    This commit introduces a new ECS system, `InstantiateLevelOnRequestSystem`, responsible for instantiating level prefabs based on incoming requests.
    
    The system performs the following actions:
    - Initializes ECS filters and pools for level components and requests.
    - In its PostRun phase, it checks for `InstantiateLevelRequest` entities.
    - If a request is found, it searches for a matching `LevelComponent` based on `LevelDto`.
    - Upon finding a match, it instantiates the level prefab, retrieves the `Level` component, and sends a `LoadLevelRequest`.
    - It updates the `LevelService` with the newly loaded level's information and sets the `Loaded` flag to true.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/InstantiateLevelOnRequestSystem.cs
@@ -0,0 +1,89 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Components;
+using _Project.Scripts.Features.Mechanics.Levels.Monos;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using _Project.Scripts.Features.Mechanics.Levels.Services;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class InstantiateLevelOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        public InstantiateLevelOnRequestSystem(LevelService levelService)
+        {
+            _levelService = levelService;
+        }
+        
+        private LevelService _levelService;
+        private EcsWorld _world;
+        private EcsFilter _requestsFilter;
+        private EcsFilter _levelsFilter;
+        private EcsPool<InstantiateLevelRequest> _instantiateLevelRequestPool;
+        private EcsPool<LevelComponent> _levelsPool;
+        private EcsPool<LoadLevelRequest> _loadLevelRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            _world = systems.GetWorld();
+
+            _requestsFilter = _world
+                .Filter<InstantiateLevelRequest>()
+                .End();
+
+            _levelsFilter = _world
+                .Filter<LevelComponent>()
+                .End();
+            
+            _instantiateLevelRequestPool = _world.GetPool<InstantiateLevelRequest>();
+            _levelsPool = _world.GetPool<LevelComponent>();
+            _loadLevelRequestPool = _world.GetPool<LoadLevelRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            if (_levelService.Loaded) return;
+            
+            foreach (var requestEntity in _requestsFilter)
+            {
+                var instantiateLevelRequest = _instantiateLevelRequestPool.Get(requestEntity);
+
+                if (TrySpawnLevel(instantiateLevelRequest)) return;
+            }
+        }
+
+        private bool TrySpawnLevel(InstantiateLevelRequest instantiateLevelRequest)
+        {
+            foreach (var levelEntity in _levelsFilter)
+            {
+                var levelComponent = _levelsPool.Get(levelEntity);
+
+                if (levelComponent.LevelDto != instantiateLevelRequest.LevelDto) continue;
+
+                SpawnLevel(levelComponent);
+                return true;
+            }
+            
+            return false;
+        }
+
+        private void SpawnLevel(LevelComponent levelComponent)
+        {
+            var levelObject = Object.Instantiate(levelComponent.Prefab);
+            var level = levelObject.GetComponent<Level>();
+            
+            if (level is not null) SendLoadRequest(level);
+
+            _levelService.Loaded = true;
+            _levelService.LevelDto = levelComponent.LevelDto;
+            _levelService.LoadedLevel = level;
+        }
+
+        private void SendLoadRequest(Level level)
+        {
+            var entity = _world.NewEntity();
+            ref var loadLevelRequest = ref _loadLevelRequestPool.Add(entity);
+            loadLevelRequest.Level = level;
+        }
+    }
+}
