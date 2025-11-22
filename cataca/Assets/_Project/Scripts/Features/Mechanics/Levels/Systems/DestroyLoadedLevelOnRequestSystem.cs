aee267844164cad07406f65378820e8671e4f959
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 22:01:56 +0300

    feat(levels): add DestroyLoadedLevelOnRequestSystem (#dev)
    This commit introduces a new system responsible for destroying the currently loaded level when a specific request is received.
    
    The `DestroyLoadedLevelOnRequestSystem` implements `IEcsInitSystem` and `IEcsPostRunSystem`.
    - In `Init`, it sets up an ECS filter to detect `DestroyLoadedLevelRequest` entities.
    - In `PostRun`, it checks if any requests are present. If so, it destroys the `LoadedLevel` object managed by `LevelService` and updates the `LevelService.Loaded` flag to false.
    
    This system is crucial for managing level transitions and freeing up resources when a level is no longer needed.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/DestroyLoadedLevelOnRequestSystem.cs
@@ -0,0 +1,37 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using _Project.Scripts.Features.Mechanics.Levels.Services;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class DestroyLoadedLevelOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        public DestroyLoadedLevelOnRequestSystem(LevelService levelService)
+        {
+            _levelService = levelService;
+        }
+        
+        private LevelService _levelService;
+        private EcsFilter _filter;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<DestroyLoadedLevelRequest>()
+                .End();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            if (_filter.GetEntitiesCount() == 0) return;
+            
+            if (_levelService.LoadedLevel != null) Object.Destroy(_levelService.LoadedLevel);
+
+            _levelService.Loaded = false;
+        }
+    }
+}
