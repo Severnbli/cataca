b2296a9a765788d2620b1e4c8e2cfddef1b7ea39
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 18:50:11 +0300

    feat(Levels): Add LevelComponent struct (#dev)
    This commit introduces a new struct `LevelComponent` to represent level data within the game.
    It includes fields for `Level` data and a `Prefab` GameObject.
    This change supports the development of new level mechanics and data management.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Components/LevelComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using _Project.Scripts.Features.Data.Entities;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Components
+{
+    [Serializable]
+    public struct LevelComponent
+    {
+        public Level Level;
+        public GameObject Prefab;
+    }
+}
