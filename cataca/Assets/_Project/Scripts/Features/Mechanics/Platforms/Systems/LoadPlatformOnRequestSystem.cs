f9585cd4006e9ca4f16a5de70f0b4e94fd151325
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 19:43:06 +0300

    feat(platforms): Add LoadPlatformOnRequestSystem (#dev)
    Introduces a new ECS system responsible for loading platform components when a load request is detected.
    This system initializes by creating a filter for `LoadPlatformRequest` and pools for
    `LoadPlatformRequest` and `PlatformComponent`. It then processes these requests in the
    PostRun phase, creating new entities with the associated `PlatformComponent`.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/LoadPlatformOnRequestSystem.cs
@@ -0,0 +1,45 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Components;
+using _Project.Scripts.Features.Mechanics.Platforms.Monos;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class LoadPlatformOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsWorld _world;
+        private EcsFilter _filter;
+        private EcsPool<LoadPlatformRequest> _loadPlatformRequestPool;
+        private EcsPool<PlatformComponent> _platformPool;
+        public void Init(IEcsSystems systems)
+        {
+            _world = systems.GetWorld();
+
+            _filter = _world
+                .Filter<LoadPlatformRequest>()
+                .End();
+            
+            _loadPlatformRequestPool = _world.GetPool<LoadPlatformRequest>();
+            _platformPool = _world.GetPool<PlatformComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var request = ref _loadPlatformRequestPool.Get(e);
+                
+                LoadPlatform(request.Platform);
+            }
+        }
+
+        private void LoadPlatform(Platform platform)
+        {
+            var entity = _world.NewEntity();
+
+            ref var platformComponent = ref _platformPool.Add(entity);
+            platformComponent.Platform = platform;
+        }
+    }
+}
