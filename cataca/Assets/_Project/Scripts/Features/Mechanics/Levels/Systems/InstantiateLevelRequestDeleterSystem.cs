37dee2dbdea419d796ca401041bf99af9ad5fec1
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 21:26:43 +0300

    feat(levels): add InstantiateLevelRequestDeleterSystem (#dev)
    This commit introduces a new ECS system responsible for deleting InstantiateLevelRequest entities.
    This system ensures that requests to instantiate levels are processed and then cleaned up
    from the ECS world, preventing potential memory leaks or unintended behavior.
    It implements IEcsInitSystem to set up the filter and pool, and IEcsRunSystem to
    iterate and delete the requests during the game loop.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/InstantiateLevelRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class InstantiateLevelRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<InstantiateLevelRequest> _instantiateLevelRequestsPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<InstantiateLevelRequest>()
+                .End();
+            
+            _instantiateLevelRequestsPool = world.GetPool<InstantiateLevelRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _instantiateLevelRequestsPool.Del(e);
+        }
+    }
+}
