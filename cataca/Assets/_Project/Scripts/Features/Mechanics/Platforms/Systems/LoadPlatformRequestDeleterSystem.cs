418c78c274df0059fff69f5b497f24b58e9d16a4
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 19:31:42 +0300

    feat(Platforms): add LoadPlatformRequestDeleterSystem (#dev)
    Introduces a new ECS system responsible for deleting LoadPlatformRequest entities.
    This system ensures that load platform requests are cleaned up after processing,
    preventing potential memory leaks or unintended behavior.
    
    The system initializes by creating a filter for entities with the LoadPlatformRequest
    component and a pool to access it. In the run phase, it iterates through all
    matching entities and deletes the LoadPlatformRequest component from them.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/LoadPlatformRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class LoadPlatformRequestDeleterSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LoadPlatformRequest> _loadPlatformRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadPlatformRequest>()
+                .End();
+            
+            _loadPlatformRequestPool = world.GetPool<LoadPlatformRequest>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _loadPlatformRequestPool.Del(e);
+        }
+    }
+}
