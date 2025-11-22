ada96b7c8f42ab577c66deff18ce5455685fb940
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 18:50:33 +0300

    feat(levels): add LevelsConfig scriptable object (#dev)
    Introduces a new ScriptableObject, LevelsConfig, to manage level data.
    This includes a list of LevelComponent objects and a validation
    method for ensuring unique level IDs and assigned prefabs.
    The validation is available in the Unity Editor.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Configs/LevelsConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _Project.Scripts._Shared.ScriptableObjects;
+using _Project.Scripts._Shared.Utils;
+using _Project.Scripts.Features.Mechanics.Levels.Components;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Configs
+{
+    public class LevelsConfig : ScriptableObjectAutoInstaller<LevelsConfig>
+    {
+        [SerializeField] private List<LevelComponent> _levels;
+        
+        public List<LevelComponent> Levels => _levels;
+      
+#if UNITY_EDITOR
+        [PropertySpace(10)]
+        [Button]
+        private void Validate()
+        {
+            var checks = EditorUtils.GetChecksContainer();
+            var ids = new HashSet<int>(_levels.Count);
+            var repeatedIds = new HashSet<int>();
+            
+            foreach (var level in _levels)
+            {
+                if (!ids.Add(level.Level.Id))
+                {
+                    repeatedIds.Add(level.Level.Id);
+                }
+                
+                checks.TryAdd(() => level.Prefab == null, $"Level with id = {level.Level.Id} has no prefab");
+            }
+            
+            checks.TryAdd(() => repeatedIds.Any(), $"Ids [{string.Join(", ", repeatedIds)}] are repeated");
+            
+            EditorUtils.Validate(checks, nameof(LevelsConfig));
+        }
+#endif
+    }
+}
