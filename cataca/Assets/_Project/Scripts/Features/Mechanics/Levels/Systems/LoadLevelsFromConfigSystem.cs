d7bbfa42227a308402208a70fb15070b0b5d90c8
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 21:20:38 +0300

    feat(levels): add system to load levels from config (#dev)
    Introduces a new ECS system responsible for loading level data from a configuration.
    This system iterates through the configured levels and creates corresponding entities
    with `LevelComponent` data, preparing the game world for level initialization.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LoadLevelsFromConfigSystem.cs
@@ -0,0 +1,31 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Components;
+using _Project.Scripts.Features.Mechanics.Levels.Configs;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class LoadLevelsFromConfigSystem : IEcsPreInitSystem, IEcsGameSystem
+    {
+        public LoadLevelsFromConfigSystem(LevelsConfig levelsConfig)
+        {
+            _levelsConfig = levelsConfig;
+        }
+        
+        private LevelsConfig _levelsConfig;
+        
+        public void PreInit(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+            var levelPool = world.GetPool<LevelComponent>();
+
+            var levels = _levelsConfig.Levels;
+            foreach (var level in levels)
+            {
+                var entity = world.NewEntity();
+                ref var levelComponent = ref levelPool.Add(entity);
+                levelComponent = level;
+            }
+        }
+    }
+}
