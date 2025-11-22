dd4768d626df645f70ba9b1bda9e6945a2461875
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-22 21:06:17 +0300

    feat(Levels): Add LoadLevelOnRequestSystem (#dev)
    Introduces a new ECS system responsible for loading levels based on requests.
    This system handles the creation of platform entities and their associated
    load requests when a level load request is processed.
    
    - Implements IEcsInitSystem, IEcsPostRunSystem, and IEcsGameSystem interfaces.
    - Filters for LoadLevelRequest to trigger level loading logic.
    - Iterates through platforms within a loaded level to create individual
      Platform entities with their own load requests.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LoadLevelOnRequestSystem.cs
@@ -0,0 +1,50 @@
+﻿using _Project.Scripts._Shared.Extensions;
+using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Monos;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using _Project.Scripts.Features.Mechanics.Platforms.Monos;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class LoadLevelOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsWorld _world;
+        private EcsFilter _filter;
+        private EcsPool<LoadLevelRequest> _loadLevelRequestPool;
+        private EcsPool<LoadPlatformRequest> _loadPlatformRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            _world = systems.GetWorld();
+
+            _filter = _world
+                .Filter<LoadLevelRequest>()
+                .End();
+            
+            _loadLevelRequestPool = _world.GetPool<LoadLevelRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                var request = _loadLevelRequestPool.Get(e);
+                LoadLevel(request.Level, e);
+            }
+        }
+
+        private void LoadLevel(Level level, int entity)
+        {
+            var platforms = level.PlatformsParent.GetChildComponents<Platform>();
+
+            foreach (var platform in platforms)
+            {
+                var platformEntity = _world.NewEntity();
+                ref var loadPlatformRequest = ref _loadPlatformRequestPool.Add(platformEntity);
+                loadPlatformRequest.Platform = platform;
+            }
+        }
+    }
+}
