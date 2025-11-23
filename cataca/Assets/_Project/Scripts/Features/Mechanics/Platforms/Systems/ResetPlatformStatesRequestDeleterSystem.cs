afc69d38eb14834987deb0ff1ad8aec54b5f8f20
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 00:48:25 +0300

    feat(platforms): add reset platform states deleter system

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Platforms/Systems/ResetPlatformStatesRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Platforms.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Platforms.Systems
+{
+    public class ResetPlatformStatesRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<ResetPlatformStatesRequest> _resetPlatformStatesRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<ResetPlatformStatesRequest>()
+                .End();
+            
+            _resetPlatformStatesRequestPool = world.GetPool<ResetPlatformStatesRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _resetPlatformStatesRequestPool.Del(e);
+        }
+    }
+}
