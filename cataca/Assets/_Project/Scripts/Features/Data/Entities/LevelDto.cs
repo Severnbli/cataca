dbe99ef990fa4dccbea6774cd1745823f75c8e87
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 02:37:36 +0300

    feat(leveldto): add equality operators and methods
    Implements `operator==`, `operator!=`, `Equals`, and `GetHashCode` for `LevelDto` to enable proper comparison based on the `Id` property.

--- cataca/Assets/_Project/Scripts/Features/Data/Entities/LevelDto.cs
+++ cataca/Assets/_Project/Scripts/Features/Data/Entities/LevelDto.cs
@@ -6,5 +6,34 @@ namespace _Project.Scripts.Features.Data.Entities
     public class LevelDto
     {
         public int Id;
+        
+        public static bool operator ==(LevelDto a, LevelDto b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+            return a.Id == b.Id;
+        }
+
+        public static bool operator !=(LevelDto a, LevelDto b)
+        {
+            return !(a == b);
+        }
+        
+        protected bool Equals(LevelDto other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((LevelDto)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() * 46;
+        }
     }
 }
