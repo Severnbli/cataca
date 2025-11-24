57d6522f22df644aa558e1630108f06fae6e5b15
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-25 00:29:23 +0300

    feat(levels): add MoveToLevelSpawnPointRequestDeleterSystem
    Introduces a new ECS system responsible for deleting `MoveToLevelSpawnPointRequest` entities after they have been processed. This ensures that requests are handled only once.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/MoveToLevelSpawnPointRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class MoveToLevelSpawnPointRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<MoveToLevelSpawnPointRequest> _moveToLevelSpawnPointRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<MoveToLevelSpawnPointRequest>()
+                .End();
+            
+            _moveToLevelSpawnPointRequestPool = world.GetPool<MoveToLevelSpawnPointRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _moveToLevelSpawnPointRequestPool.Del(e);
+        }
+    }
+}
