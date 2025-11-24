120a7a4e8389abd0f31a1551f438eddd8d29f76c
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 03:30:44 +0300

    feat(groundcheck): add GroundCheckResultComponent struct
    Introduces a new struct `GroundCheckResultComponent` to store ground check results, including the transform and offset.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Characters/Checks/Components/GroundCheckResultComponent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Characters.Checks.Components
+{
+    public struct GroundCheckResultComponent
+    {
+        public Transform Transform;
+        public Vector3 Offset;
+    }
+}
