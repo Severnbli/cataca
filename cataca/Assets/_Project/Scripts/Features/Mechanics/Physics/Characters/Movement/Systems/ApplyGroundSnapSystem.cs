9dfe91213c4560f6862d5e16ff3bfbb01efe619d
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 09:53:50 +0300

    feat(movement): implement ground snap system
    Introduces a new ECS system to snap character transforms to the ground based on ground check results. This system ensures characters remain grounded by adjusting their position relative to the detected ground.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Characters/Movement/Systems/ApplyGroundSnapSystem.cs
@@ -0,0 +1,38 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features._Shared.Components;
+using _Project.Scripts.Features.Mechanics.Physics.Characters.Checks.Components;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Characters.Movement.Systems
+{
+    public class ApplyGroundSnapSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<GroundCheckResultComponent> _groundCheckResultPool;
+        private EcsPool<TransformComponent> _transformPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<GroundCheckResultComponent>()
+                .Inc<TransformComponent>()
+                .End();
+            
+            _groundCheckResultPool = world.GetPool<GroundCheckResultComponent>();
+            _transformPool = world.GetPool<TransformComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var groundCheckResult = ref _groundCheckResultPool.Get(e);
+                ref var transform = ref _transformPool.Get(e);
+                
+                transform.Transform.position = groundCheckResult.Transform.position - groundCheckResult.Offset;
+            }
+        }
+    }
+}
