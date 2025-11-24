0afca7963f9f05eb3e48e01755d76e04b272aecd
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 12:44:01 +0300

    feat(levels): add level button update system
    Introduces a new ECS system responsible for updating level button UI elements.
    This system iterates through entities with LevelButtonComponent and
    sets the text of the level button's name based on the LevelDto data.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LevelButtonUpdateSystem.cs
@@ -0,0 +1,36 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Components;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class LevelButtonUpdateSystem : IEcsInitSystem, IEcsRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LevelButtonComponent> _levelButtonPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LevelButtonComponent>()
+                .End();
+            
+            _levelButtonPool = world.GetPool<LevelButtonComponent>();
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var levelButtonComponent = ref _levelButtonPool.Get(e);
+                var levelButton = levelButtonComponent.LevelButton;
+                
+                if (levelButton.LevelDto is null) continue;
+                
+                levelButton.Name.text = levelButton.LevelDto.Name;
+            }
+        }
+    }
+}
