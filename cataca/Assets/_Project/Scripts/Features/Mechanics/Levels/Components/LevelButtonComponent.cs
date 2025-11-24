01dfaac077fd1eb14befb75da630046e8453d9ac
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 10:39:07 +0300

    feat(levels): add LevelButtonComponent
    Introduces a new struct `LevelButtonComponent` to represent level button data. This component will be used to associate level button logic with specific UI elements.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Components/LevelButtonComponent.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Levels.Monos;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Components
+{
+    public struct LevelButtonComponent
+    {
+        public LevelButton LevelButton;
+    }
+}
