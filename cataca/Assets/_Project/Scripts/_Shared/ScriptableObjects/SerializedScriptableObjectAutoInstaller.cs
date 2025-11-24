31e4661dcd91ccbc929d2f450fc1f679b38fcbf3
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 18:17:27 +0300

    feat(scriptableobjects): add auto installer for serialized scriptable objects
    Introduces a new generic base class, `SerializedScriptableObjectAutoInstaller<T>`, which automatically handles serialization and deserialization for `SerializedScriptableObject` instances. This simplifies the process of saving and loading custom scriptable object data.

---
+++ cataca/Assets/_Project/Scripts/_Shared/ScriptableObjects/SerializedScriptableObjectAutoInstaller.cs
@@ -0,0 +1,48 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace _Project.Scripts._Shared.ScriptableObjects
+{
+    [ShowOdinSerializedPropertiesInInspector]
+    public class SerializedScriptableObjectAutoInstaller<T> : ScriptableObjectAutoInstaller<T>, ISerializationCallbackReceiver
+        where T : SerializedScriptableObjectAutoInstaller<T>
+    {
+        [SerializeField]
+        [HideInInspector]
+        private SerializationData serializationData;
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            if (this.SafeIsUnityNull())
+                return;
+            UnitySerializationUtility.DeserializeUnityObject(this, ref serializationData);
+            OnAfterDeserialize();
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            if (this.SafeIsUnityNull())
+                return;
+            OnBeforeSerialize();
+            UnitySerializationUtility.SerializeUnityObject(this, ref serializationData);
+        }
+
+        protected virtual void OnAfterDeserialize()
+        {
+        }
+
+        protected virtual void OnBeforeSerialize()
+        {
+        }
+
+        [HideInTables]
+        [OnInspectorGUI]
+        [PropertyOrder(-2.1474836E+09f)]
+        private void InternalOnInspectorGUI()
+        {
+            EditorOnlyModeConfigUtility.InternalOnInspectorGUI(this);
+        }
+    }
+}
