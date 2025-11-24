0ef9beee14d65d82ed267b8a0d4ede732c818345
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 18:18:28 +0300

    feat(scenes): add LoadSceneRequest struct
    Introduces a new struct `LoadSceneRequest` to encapsulate scene loading requests. This struct will be used to pass scene information for loading operations.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Scenes/Requests/LoadSceneRequest.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Scenes.Enums;
+
+namespace _Project.Scripts.Features.Mechanics.Scenes.Requests
+{
+    public struct LoadSceneRequest
+    {
+        public Scene Scene;
+    }
+}
