07c0694ec15c074180506ade2ef056bb95774a1a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 12:31:58 +0300

    feat(levels): add system to load level button on request
    Introduces a new ECS system, `LoadLevelButtonOnRequestSystem`, which handles
    the initialization of level button components based on incoming requests.
    This system ensures that UI buttons designated for level loading are
    correctly configured when a `LoadLevelButtonRequest` is received.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LoadLevelButtonOnRequestSystem.cs
@@ -0,0 +1,43 @@
+﻿using _Project.Scripts._Shared.Extensions;
+using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Components;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using _Project.Scripts.Features.UI.Buttons.Components;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class LoadLevelButtonOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LoadLevelButtonRequest> _loadLevelButtonRequestPool;
+        private EcsPool<LevelButtonComponent> _levelButtonPool;
+        private EcsPool<ButtonComponent> _buttonPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadLevelButtonRequest>()
+                .End();
+            
+            _loadLevelButtonRequestPool = world.GetPool<LoadLevelButtonRequest>();
+            _levelButtonPool = world.GetPool<LevelButtonComponent>();
+            _buttonPool = world.GetPool<ButtonComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var request = ref _loadLevelButtonRequestPool.Get(e);
+                ref var levelButton = ref _levelButtonPool.AddComponentIfNotExists(e);
+                ref var button = ref _buttonPool.AddComponentIfNotExists(e);
+
+                levelButton.LevelButton = request.LevelButton;
+                button.Button = request.LevelButton.Button;
+            }
+        }
+    }
+}
