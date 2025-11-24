2d1d6633e3b1c66b62e2a7a01e20e241ea30bd77
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 21:08:33 +0300

    feat(scenes): add load scene on request system
    Introduces a new ECS system responsible for loading scenes based on requests.
    This system listens for `LoadSceneRequest` events and uses a `ScenesConfig`
    to map scene identifiers to actual scene names for loading.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Scenes/Systems/LoadSceneOnRequestSystem.cs
@@ -0,0 +1,42 @@
+﻿using _Project.Scripts._Shared.Extensions;
+using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Scenes.Configs;
+using _Project.Scripts.Features.Mechanics.Scenes.Requests;
+using Leopotam.EcsLite;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Features.Mechanics.Scenes.Systems
+{
+    public class LoadSceneOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        public LoadSceneOnRequestSystem(ScenesConfig scenesConfig)
+        {
+            _scenesConfig = scenesConfig;
+        }
+        
+        private ScenesConfig _scenesConfig;
+        private EcsFilter _filter;
+        private EcsPool<LoadSceneRequest> _loadSceneRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadSceneRequest>()
+                .End();
+            
+            _loadSceneRequestPool = world.GetPool<LoadSceneRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            if (!_filter.TryGetFirst(out var entity)) return;
+            
+            ref var request = ref _loadSceneRequestPool.Get(entity);
+            if (!_scenesConfig.Scenes.TryGetValue(request.Scene, out var sceneName)) return;
+
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
