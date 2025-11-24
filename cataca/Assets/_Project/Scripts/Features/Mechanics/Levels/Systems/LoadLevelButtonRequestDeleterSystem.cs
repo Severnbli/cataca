479155616a093270a8ca3161a8f27ee87c80c328
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 10:42:05 +0300

    feat(levels): add LoadLevelButtonRequestDeleterSystem
    Introduces a new ECS system responsible for deleting `LoadLevelButtonRequest` entities after they have been processed. This ensures that requests are handled only once.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LoadLevelButtonRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Levels.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Systems
+{
+    public class LoadLevelButtonRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LoadLevelButtonRequest> _loadLevelButtonRequestPool; 
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadLevelButtonRequest>()
+                .End();
+            
+            _loadLevelButtonRequestPool = world.GetPool<LoadLevelButtonRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _loadLevelButtonRequestPool.Del(e);
+        }
+    }
+}
