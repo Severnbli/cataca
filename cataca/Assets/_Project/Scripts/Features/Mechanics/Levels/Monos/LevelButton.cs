a2c919f17eb919b1c4d7c4531b2068ddd4d5ffbf
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 10:07:03 +0300

    feat(levels): add LevelButton component
    Introduces a new MonoBehaviour script, LevelButton, to manage level button UI elements. This component holds references to a ButtonWrapper and a TextMeshProUGUI for displaying the level name.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/LevelButton.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Features.UI.Buttons.Monos;
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Monos
+{
+    public class LevelButton : MonoBehaviour
+    {
+        [SerializeField] private ButtonWrapper _button;
+        [SerializeField] private TextMeshProUGUI _name;
+        
+        public ButtonWrapper Button => _button;
+        public TextMeshProUGUI Name => _name;
+    }
+}
