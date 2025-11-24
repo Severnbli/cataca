d5a44615e0da861e9a6dbd1df5c03b4dc0441e7a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 10:07:15 +0300

    feat(levels): add LevelsUIContainerMarkerConverter
    Introduces a new converter for LevelsUIContainerMarker, enabling its use in entity-component systems.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Converters/LevelsUIContainerMarkerConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.Mechanics.Levels.Markers;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Converters
+{
+    public class LevelsUIContainerMarkerConverter : ComponentConverter<LevelsUIContainerMarker>
+    {
+        
+    }
+}
