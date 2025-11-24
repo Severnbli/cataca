da5fb444185166330fc31f42907549e14a81aae4
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 10:39:51 +0300

    feat(levels): add LoadLevelButtonRequest struct
    Introduces a new struct to represent requests for loading level buttons. This will be used to decouple UI interactions from level loading logic.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Requests/LoadLevelButtonRequest.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Levels.Monos;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Requests
+{
+    public struct LoadLevelButtonRequest
+    {
+        public LevelButton LevelButton;
+    }
+}
