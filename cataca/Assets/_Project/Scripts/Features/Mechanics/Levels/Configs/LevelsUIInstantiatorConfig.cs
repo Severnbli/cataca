fdfeedc985ed1d7af1c3eabaefb33fe91b0e7e08
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 10:06:02 +0300

    feat(levels): add LevelsUIInstantiatorConfig scriptable object
    Introduces a new ScriptableObject for configuring level UI instantiation.
    This allows for centralized management of level UI prefabs.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Configs/LevelsUIInstantiatorConfig.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Scripts._Shared.ScriptableObjects;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Configs
+{
+    public class LevelsUIInstantiatorConfig : ScriptableObjectAutoInstaller<LevelsUIInstantiatorConfig>
+    {
+        [SerializeField] private GameObject _prefab;
+        
+        public GameObject Prefab => _prefab;
+    }
+}
