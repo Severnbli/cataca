5a44d70bccc4775421465c91a9ea033db4da9eda
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-25 00:18:15 +0300

    feat(level): add player spawn point field
    - Introduces a new serialized field `_playerSpawnPoint` to the `Level` MonoBehaviour.
    - Adds a public property `PlayerSpawnPoint` to access the spawn point transform.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
@@ -5,7 +5,9 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Monos
     public class Level : MonoBehaviour
     {
         [SerializeField] private GameObject _platformsParent;
+        [SerializeField] private Transform _playerSpawnPoint;
         
         public GameObject PlatformsParent => _platformsParent;
+        public Transform PlayerSpawnPoint => _playerSpawnPoint;
     }
 }
