a91992ff58ba59383d38faa15fd2270af757f00a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-24 21:14:08 +0300

    feat(scene): add SceneLoaderComponent struct
    Introduces a new struct `SceneLoaderComponent` to hold scene loading information.
    This component will be used in conjunction with scene loading systems.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Scenes/Components/SceneLoaderComponent.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Scenes.Enums;
+
+namespace _Project.Scripts.Features.Mechanics.Scenes.Components
+{
+    public struct SceneLoaderComponent
+    {
+        public Scene Scene;
+    }
+}
