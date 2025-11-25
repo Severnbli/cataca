b51ec45636adafc60f7354a2d78f0a10841e5597
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-25 06:24:33 +0300

    feat(scene): add scene loader component and converter
    Introduces a new `SceneLoaderComponent` and its corresponding `SceneLoaderConverter`. This allows for scene loading logic to be integrated with the entity-component system. The component is marked as `[Serializable]` to ensure proper serialization.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Scenes/Converters/SceneLoaderConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.Mechanics.Scenes.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Mechanics.Scenes.Converters
+{
+    public class SceneLoaderConverter : ComponentConverter<SceneLoaderComponent>
+    {
+        
+    }
+}
