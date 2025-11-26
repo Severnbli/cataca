5a0c16726f8d8f603df826150b0a4e6cf96ed495
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-26 23:07:11 +0300

    feat(utils): add DOTween time calculation helpers
    Introduces a new static class `DOTweenUtils` with methods to calculate the time required for DOTween animations based on distance and speed for both Vector3 and Quaternion types.

---
+++ cataca/Assets/_Project/Scripts/_Shared/Utils/DOTweenUtils.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts._Shared.Utils
+{
+    public static class DOTweenUtils
+    {
+        public static float GetTimeToChange(Vector3 current, Vector3 target, float speed)
+        {
+            var distance = Vector3.Distance(current, target);
+            return distance / speed;
+        }
+
+        public static float GetTimeToChange(Quaternion current, Quaternion target, float speed)
+        {
+            var distance = Quaternion.Angle(current, target);
+            return distance / speed;
+        }
+    }
+}
