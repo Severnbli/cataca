5e58799004c4b378688e3d7d0ee499df7fad987a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-27 02:54:10 +0300

    fix(physics): move ColliderComponent and its converter
    The ColliderComponent and its associated converter have been moved from a shared
    components folder to a dedicated Colliders folder. This change improves
    organization and modularity within the physics features.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Colliders/Converters/ColliderConverter.cs
@@ -0,0 +1,10 @@
+﻿using _Project.Scripts.Features.Mechanics.Physics.Colliders.Components;
+using AB_Utility.FromSceneToEntityConverter;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Colliders.Converters
+{
+    public class ColliderConverter : ComponentConverter<ColliderComponent>
+    {
+        
+    }
+}
--- cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/_Shared/Converters/ColliderComponentConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using _Project.Scripts.Features.Mechanics.Physics._Shared.Components;
-using AB_Utility.FromSceneToEntityConverter;
-
-namespace _Project.Scripts.Features.Mechanics.Physics._Shared.Converters
-{
-    public class ColliderComponentConverter : ComponentConverter<ColliderComponent>
-    {
-    }
-}
