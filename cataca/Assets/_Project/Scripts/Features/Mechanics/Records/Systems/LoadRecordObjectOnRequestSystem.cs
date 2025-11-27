32896a742f075a420258b13f6fc402471f1b5844
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 01:06:15 +0300

    feat(records): add system to load record object on request
    Introduces a new ECS system that handles the loading of record objects when a `LoadRecordObjectRequest` is detected. It ensures that `RecordObjectComponent` and `ColliderComponent` are added and populated correctly.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Systems/LoadRecordObjectOnRequestSystem.cs
@@ -0,0 +1,44 @@
+﻿using _Project.Scripts._Shared.Extensions;
+using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Physics.Colliders.Components;
+using _Project.Scripts.Features.Mechanics.Records.Components;
+using _Project.Scripts.Features.Mechanics.Records.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Systems
+{
+    public class LoadRecordObjectOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LoadRecordObjectRequest> _loadRecordObjectRequestPool;
+        private EcsPool<RecordObjectComponent> _recordObjectPool;
+        private EcsPool<ColliderComponent> _colliderPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadRecordObjectRequest>()
+                .End();
+            
+            _loadRecordObjectRequestPool = world.GetPool<LoadRecordObjectRequest>();
+            _recordObjectPool = world.GetPool<RecordObjectComponent>();
+            _colliderPool = world.GetPool<ColliderComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var request = ref _loadRecordObjectRequestPool.Get(e);
+                
+                ref var recordObject = ref _recordObjectPool.AddComponentIfNotExists(e);
+                recordObject.RecordObject = request.RecordObject;
+                
+                ref var collider = ref _colliderPool.AddComponentIfNotExists(e);
+                collider.Collider = recordObject.RecordObject.Collider;
+            }
+        }
+    }
+}
