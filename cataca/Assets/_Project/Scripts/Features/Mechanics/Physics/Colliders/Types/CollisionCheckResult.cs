c0ca8930d958d1b1245ff2faed61353de824fd0a
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-27 09:41:38 +0300

    feat(physics): add CollisionCheckResult struct
    Introduces a new struct to encapsulate collision check outcomes,
    including status and distance information. This will be used
    by the physics system for collision detection.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Colliders/Types/CollisionCheckResult.cs
@@ -0,0 +1,11 @@
+﻿using _Project.Scripts.Features.Mechanics.Physics.Colliders.Enums;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Colliders.Types
+{
+    public struct CollisionCheckResult
+    {
+        public CollisionCheckStatus CheckStatus;
+        public ColliderDistance2D Distance;
+    }
+}
