3c87ae1eab321bf9d9ecaca5bbf8bc554d7ad6b3
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-27 10:04:23 +0300

    refactor(collider): expose collider fields
    Changed private readonly fields _a and _b to public readonly properties A and B in ColliderPair struct. This change improves encapsulation and allows external access to the collider references without altering the struct's functional behavior.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Colliders/Types/ColliderPair.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Colliders/Types/ColliderPair.cs
@@ -5,33 +5,33 @@ namespace _Project.Scripts.Features.Mechanics.Physics.Colliders.Types
 {
     public readonly struct ColliderPair : IEquatable<ColliderPair>
     {
-        private readonly Collider2D _a;
-        private readonly Collider2D _b;
+        public readonly Collider2D A;
+        public readonly Collider2D B;
 
         public ColliderPair(Collider2D a, Collider2D b)
         {
             if (a.GetHashCode() <= b.GetHashCode())
             {
-                _a = a;
-                _b = b;
+                A = a;
+                B = b;
             }
             else
             {
-                _a = b;
-                _b = a;
+                A = b;
+                B = a;
             }
         }
 
         public bool Equals(ColliderPair other) 
-            => (_a == other._a && _b == other._b) || (_a == other._b && _b == other._a);
+            => (A == other.A && B == other.B) || (A == other.B && B == other.A);
         
         public override bool Equals(object obj)
             => obj is ColliderPair other && Equals(other);
 
         public override int GetHashCode()
         {
-            var hashA = _a != null ? _a.GetHashCode() : 0;
-            var hashB = _b != null ? _b.GetHashCode() : 0;
+            var hashA = A != null ? A.GetHashCode() : 0;
+            var hashB = B != null ? B.GetHashCode() : 0;
             return HashCode.Combine(hashA < hashB ? hashA : hashB, hashA < hashB ? hashB : hashA);
         }
     }
