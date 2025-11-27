64b1f3262f0b4d1da08bee6a13e6c531d5032a82
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 01:47:33 +0300

    feat(level): add records parent field
    Introduces a new serialized field `_recordsParent` and its corresponding public getter `RecordsParent` to the `Level` MonoBehaviour. This prepares the Level script to manage record objects.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
@@ -5,9 +5,11 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Monos
     public class Level : MonoBehaviour
     {
         [SerializeField] private GameObject _platformsParent;
+        [SerializeField] private GameObject _recordsParent;
         [SerializeField] private Transform _spawnPoint;
         
         public GameObject PlatformsParent => _platformsParent;
+        public GameObject RecordsParent => _recordsParent;
         public Transform SpawnPoint => _spawnPoint;
     }
 }
