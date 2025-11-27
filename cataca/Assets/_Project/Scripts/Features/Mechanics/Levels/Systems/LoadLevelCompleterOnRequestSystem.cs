d92b9f363d0b6b9d1eae773d54f739a40c3a8a48
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 02:32:00 +0300

    fix(levels): add scene loader component to level completer
    - Initializes SceneLoaderComponent for level completer entities.
    - Sets the scene to `Scene.Menu` by default.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LoadLevelCompleterOnRequestSystem.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Systems/LoadLevelCompleterOnRequestSystem.cs
@@ -4,6 +4,8 @@ using _Project.Scripts.Features.Mechanics.Levels.Components;
 using _Project.Scripts.Features.Mechanics.Levels.Markers;
 using _Project.Scripts.Features.Mechanics.Levels.Requests;
 using _Project.Scripts.Features.Mechanics.Physics.Colliders.Components;
+using _Project.Scripts.Features.Mechanics.Scenes.Components;
+using _Project.Scripts.Features.Mechanics.Scenes.Enums;
 using Leopotam.EcsLite;
 
 namespace _Project.Scripts.Features.Mechanics.Levels.Systems
@@ -14,8 +16,9 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Systems
         private EcsPool<LoadLevelCompleterRequest> _loadLevelCompleterRequestPool;
         private EcsPool<LevelCompleterComponent> _levelCompleterPool;
         private EcsPool<ColliderComponent> _colliderPool;
+        private EcsPool<SceneLoaderComponent> _sceneLoaderPool;
         private EcsPool<DelEntityOnDestroyLevelMarker> _delEntityOnDestroyMarkerPool;
-        
+
         public void Init(IEcsSystems systems)
         {
             var world = systems.GetWorld();
@@ -27,6 +30,7 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Systems
             _loadLevelCompleterRequestPool = world.GetPool<LoadLevelCompleterRequest>();
             _levelCompleterPool = world.GetPool<LevelCompleterComponent>();
             _colliderPool = world.GetPool<ColliderComponent>();
+            _sceneLoaderPool = world.GetPool<SceneLoaderComponent>();
             _delEntityOnDestroyMarkerPool = world.GetPool<DelEntityOnDestroyLevelMarker>();
         }
 
@@ -41,6 +45,9 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Systems
                 
                 ref var collider = ref _colliderPool.AddComponentIfNotExists(e);
                 collider.Collider = request.LevelCompleter.Collider;
+                
+                ref var sceneLoader = ref _sceneLoaderPool.AddComponentIfNotExists(e);
+                sceneLoader.Scene = Scene.Menu;
 
                 _delEntityOnDestroyMarkerPool.AddComponentIfNotExists(e);
             }
