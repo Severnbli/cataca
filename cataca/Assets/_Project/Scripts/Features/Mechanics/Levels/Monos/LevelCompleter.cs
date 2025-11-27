664f0c29b4ec53c4c55455ce5bcbf65e524b3e24
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 02:21:04 +0300

    feat(level): add LevelCompleter component
    Introduces a new MonoBehaviour script, `LevelCompleter`, to manage level completion logic. It includes a serialized field for a `Collider2D`.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/LevelCompleter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Levels.Monos
+{
+    public class LevelCompleter : MonoBehaviour
+    {
+        [SerializeField] private Collider2D _collider;
+        
+        public Collider2D Collider => _collider;
+    }
+}
