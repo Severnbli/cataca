5ed8cf93fdb39341596f502293ecfaf8bc0ef4ac
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 02:21:10 +0300

    fix(Level): use LevelCompleter type instead of Collider2D

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
@@ -7,11 +7,11 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Monos
         [SerializeField] private GameObject _platformsParent;
         [SerializeField] private GameObject _recordsParent;
         [SerializeField] private Transform _spawnPoint;
-        [SerializeField] private Collider2D _levelCompleter;
+        [SerializeField] private LevelCompleter _levelCompleter;
         
         public GameObject PlatformsParent => _platformsParent;
         public GameObject RecordsParent => _recordsParent;
         public Transform SpawnPoint => _spawnPoint;
-        public Collider2D LevelCompleter => _levelCompleter;
+        public LevelCompleter LevelCompleter => _levelCompleter;
     }
 }
