47db8be09688dbcebaafae580344269f839c86f3
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-27 09:41:47 +0300

    feat(physics): introduce CollisionService
    Creates a new CollisionService class to manage collision data.
    This service will store collision information using a dictionary.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Physics/Colliders/Services/CollisionService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using _Project.Scripts.Features.Mechanics.Physics.Colliders.Types;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Physics.Colliders.Services
+{
+    public sealed class CollisionService
+    {
+        public Dictionary<KeyValuePair<Collider2D, Collider2D>, CollisionCheckResult> Collisions { get; private set; } = new();
+    }
+}
