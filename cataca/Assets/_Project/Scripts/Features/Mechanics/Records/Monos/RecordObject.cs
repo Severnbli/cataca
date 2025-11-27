621fc4cfc7b6f5f4daaf7230c4c78b859789ed3b
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 00:56:18 +0300

    feat(records): add collider and id properties
    - Expose `_recordId` as `RecordId` property.
    - Expose `_collider` as `Collider` property.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Monos/RecordObject.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Monos/RecordObject.cs
@@ -6,5 +6,9 @@ namespace _Project.Scripts.Features.Mechanics.Records.Monos
     public class RecordObject : SerializedMonoBehaviour
     {
         [SerializeField] private int _recordId;
+        [SerializeField] private Collider2D _collider;
+        
+        public int RecordId => _recordId;
+        public Collider2D Collider => _collider;
     }
 }
