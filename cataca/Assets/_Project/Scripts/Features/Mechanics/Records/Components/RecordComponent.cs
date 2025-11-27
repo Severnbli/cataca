5b320dbc00c3d2cb2607e9f95603cb4cd3db1f0e
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 00:56:26 +0300

    feat(records): add RecordComponent struct
    Introduces a new struct `RecordComponent` to hold a `RecordObject`. This is a foundational element for the record mechanics feature.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Components/RecordComponent.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Records.Monos;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Components
+{
+    public struct RecordComponent
+    {
+        public RecordObject Record;
+    }
+}
