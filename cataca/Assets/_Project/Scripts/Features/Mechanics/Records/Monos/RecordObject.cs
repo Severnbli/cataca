113b1836a63f6e8d9f75f7f5f0db17f91d9c0859
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 00:43:25 +0300

    feat(records): add RecordObject MonoBehaviour
    Introduces a new MonoBehaviour script, `RecordObject`, for managing record data.
    It includes a serialized field for `_recordId`.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Monos/RecordObject.cs
@@ -0,0 +1,10 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Monos
+{
+    public class RecordObject : SerializedMonoBehaviour
+    {
+        [SerializeField] private int _recordId;
+    }
+}
