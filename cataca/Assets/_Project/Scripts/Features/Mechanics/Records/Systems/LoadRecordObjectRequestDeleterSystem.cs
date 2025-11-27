a4876539301dcb54e3710730a1fc9fa19a9d5e56
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 01:01:02 +0300

    feat(records): add LoadRecordObjectRequestDeleterSystem
    Introduces a new ECS system responsible for deleting LoadRecordObjectRequest entities after they have been processed. This ensures that requests are handled only once and prevents potential issues with stale data.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Records/Systems/LoadRecordObjectRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Records.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Records.Systems
+{
+    public class LoadRecordObjectRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LoadRecordObjectRequest> _loadRecordObjectRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadRecordObjectRequest>()
+                .End();
+            
+            _loadRecordObjectRequestPool = world.GetPool<LoadRecordObjectRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _loadRecordObjectRequestPool.Del(e);
+        }
+    }
+}
