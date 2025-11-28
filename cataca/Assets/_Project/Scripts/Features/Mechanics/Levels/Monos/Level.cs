77221d5c9b3a9c7e85b9ba399c8fe93dd2096bcd
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 10:58:06 +0300

    feat(Level): add dangers parent field
    Introduces a new serialized field `_dangersParent` and its corresponding public getter `DangersParent` to the `Level` script. This prepares for the management of danger elements within levels.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Levels/Monos/Level.cs
@@ -6,11 +6,13 @@ namespace _Project.Scripts.Features.Mechanics.Levels.Monos
     {
         [SerializeField] private GameObject _platformsParent;
         [SerializeField] private GameObject _recordsParent;
+        [SerializeField] private GameObject _dangersParent;
         [SerializeField] private Transform _spawnPoint;
         [SerializeField] private LevelCompleter _levelCompleter;
         
         public GameObject PlatformsParent => _platformsParent;
         public GameObject RecordsParent => _recordsParent;
+        public GameObject DangersParent => _dangersParent;
         public Transform SpawnPoint => _spawnPoint;
         public LevelCompleter LevelCompleter => _levelCompleter;
     }
