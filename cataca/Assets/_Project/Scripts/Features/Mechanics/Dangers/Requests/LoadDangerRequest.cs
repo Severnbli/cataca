c7ca439dac5ed3ca099ead5b75645501491dcc51
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 10:49:13 +0300

    feat(dangers): add LoadDangerRequest struct
    Introduces a new struct for loading danger requests. This struct will be used to encapsulate data related to loading danger entities within the game.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Dangers/Requests/LoadDangerRequest.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.Features.Mechanics.Dangers.Monos;
+
+namespace _Project.Scripts.Features.Mechanics.Dangers.Requests
+{
+    public struct LoadDangerRequest
+    {
+        public Danger Danger;
+    }
+}
