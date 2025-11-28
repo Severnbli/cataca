e9c161b17d485bc696ad2335f46f0cd98ae087df
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 11:18:38 +0300

    feat(player): add system to play death animation
    Introduces a new ECS system responsible for triggering the player's death animation.
    This system listens for `PlayDeathAnimationRequest` and sets the death trigger on the
    `AnimatorComponent`.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/PlayDeathAnimationOnRequestSystem.cs
@@ -0,0 +1,40 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Anims.Components;
+using _Project.Scripts.Features.Mechanics.Player.Configs;
+using _Project.Scripts.Features.Mechanics.Player.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Player.Systems
+{
+    public class PlayDeathAnimationOnRequestSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        public PlayDeathAnimationOnRequestSystem(PlayerAnimationConfig animationConfig)
+        {
+            _animationConfig = animationConfig;
+        }
+
+        private PlayerAnimationConfig _animationConfig;
+        private EcsFilter _filter;
+        private EcsPool<AnimatorComponent> _animatorPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<PlayDeathAnimationRequest>()
+                .End();
+            
+            _animatorPool = world.GetPool<AnimatorComponent>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter)
+            {
+                ref var animator = ref _animatorPool.Get(e);
+                animator.Animator.SetTrigger(_animationConfig.DeathTrigger);
+            }
+        }
+    }
+}
