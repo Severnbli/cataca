418fa9cf3d690fa22a5e7dd4dd58b74f5fb82c38
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 23:04:24 +0300

    fix(player): update fall animation logic with movement config
    - Added PlayerMovementConfig dependency to PlayerFallAnimationUpdateSystem.
    - Updated GetIsFalling to use movementConfig.FallThreshold for more accurate
      fall detection.
    - Removed redundant check for RigidbodyComponent in GetIsFalling.

--- cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/PlayerFallAnimationUpdateSystem.cs
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/PlayerFallAnimationUpdateSystem.cs
@@ -10,12 +10,14 @@ namespace _Project.Scripts.Features.Mechanics.Player.Systems
 {
     public class PlayerFallAnimationUpdateSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
     {
-        public PlayerFallAnimationUpdateSystem(PlayerAnimationConfig config)
+        public PlayerFallAnimationUpdateSystem(PlayerAnimationConfig animationConfig, PlayerMovementConfig movementConfig)
         {
-            _config = config;
+            _animationConfig = animationConfig;
+            _movementConfig = movementConfig;
         }
         
-        private PlayerAnimationConfig _config;
+        private PlayerAnimationConfig _animationConfig;
+        private PlayerMovementConfig _movementConfig;
         private EcsFilter _filter;
         private EcsPool<AnimatorComponent> _animatorPool;
         private EcsPool<RigidbodyComponent> _rigidbodyPool;
@@ -41,22 +43,22 @@ namespace _Project.Scripts.Features.Mechanics.Player.Systems
             {
                 var isFalling = GetIsFalling(e);
                 ref var animator = ref _animatorPool.Get(e);
-                animator.Animator.SetBool(_config.IsFalling, isFalling);
+                animator.Animator.SetBool(_animationConfig.IsFalling, isFalling);
             }
         }
 
         private bool GetIsFalling(int entity)
         {
+            if (_rigidbodyPool.Has(entity))
+            {
+                ref var rigidbody = ref _rigidbodyPool.Get(entity);
+                return rigidbody.Rigidbody.linearVelocity.y + _movementConfig.FallThreshold < 0;
+            }
+            
             if (_groundCheckStatusPool.Has(entity))
             {
                 ref var groundCheckStatus = ref _groundCheckStatusPool.Get(entity);
                 return !groundCheckStatus.IsOnGround;
-            }
-            
-            if (_rigidbodyPool.Has(entity))
-            {
-                ref var rigidbody = ref _rigidbodyPool.Get(entity);
-                return rigidbody.Rigidbody.linearVelocity.y < 0;
             }
             
             return false;
