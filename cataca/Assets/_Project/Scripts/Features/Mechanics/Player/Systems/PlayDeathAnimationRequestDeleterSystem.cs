d0d7e1280f5ec07cfc3f1939020446963a911b27
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 11:13:45 +0300

    feat(player): add system to delete death animation requests
    Introduces a new ECS system responsible for cleaning up `PlayDeathAnimationRequest` entities after they have been processed. This ensures that animation requests are handled only once.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/PlayDeathAnimationRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Player.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Player.Systems
+{
+    public class PlayDeathAnimationRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<PlayDeathAnimationRequest> _playDeathAnimationRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<PlayDeathAnimationRequest>()
+                .End();
+            
+            _playDeathAnimationRequestPool = world.GetPool<PlayDeathAnimationRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _playDeathAnimationRequestPool.Del(e);
+        }
+    }
+}
