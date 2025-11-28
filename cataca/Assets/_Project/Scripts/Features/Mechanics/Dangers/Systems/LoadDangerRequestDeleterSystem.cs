daad0837b61319ca8d6624414223db1d417563f5
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 10:52:53 +0300

    feat(dangers): add LoadDangerRequestDeleterSystem
    Introduces a new ECS system responsible for deleting `LoadDangerRequest` entities after they have been processed. This ensures that requests are handled only once and prevents potential resource leaks or incorrect behavior.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Dangers/Systems/LoadDangerRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Dangers.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Dangers.Systems
+{
+    public class LoadDangerRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<LoadDangerRequest> _loadDangerRequestDeleter;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<LoadDangerRequest>()
+                .End();
+            
+            _loadDangerRequestDeleter = world.GetPool<LoadDangerRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _loadDangerRequestDeleter.Del(e);
+        }
+    }
+}
