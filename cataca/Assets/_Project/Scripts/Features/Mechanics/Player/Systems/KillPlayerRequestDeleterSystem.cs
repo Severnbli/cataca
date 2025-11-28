7fc15941b27873075c096d582ee5f6effae5672c
Usevalad Buben <usevaladbuben@gmail.com>
2025-11-28 11:06:14 +0300

    feat(player): add kill request deleter system
    Introduces a new ECS system responsible for deleting `KillPlayerRequest` entities after they have been processed. This ensures that requests are handled only once.

---
+++ cataca/Assets/_Project/Scripts/Features/Mechanics/Player/Systems/KillPlayerRequestDeleterSystem.cs
@@ -0,0 +1,28 @@
+﻿using _Project.Scripts.Core.Systems.Interfaces;
+using _Project.Scripts.Features.Mechanics.Player.Requests;
+using Leopotam.EcsLite;
+
+namespace _Project.Scripts.Features.Mechanics.Player.Systems
+{
+    public class KillPlayerRequestDeleterSystem : IEcsInitSystem, IEcsPostRunSystem, IEcsGameSystem
+    {
+        private EcsFilter _filter;
+        private EcsPool<KillPlayerRequest> _killPlayerRequestPool;
+        
+        public void Init(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+
+            _filter = world
+                .Filter<KillPlayerRequest>()
+                .End();
+            
+            _killPlayerRequestPool = world.GetPool<KillPlayerRequest>();
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            foreach (var e in _filter) _killPlayerRequestPool.Del(e);
+        }
+    }
+}
